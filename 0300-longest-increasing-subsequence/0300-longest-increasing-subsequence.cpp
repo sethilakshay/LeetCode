df82a0888057723a52364dbdc9279d8f147df3a1
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-10-23 18:23:06 -0400

    Time: 14 ms (89.72%), Space: 10.5 MB (71.39%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //TC: O(N logN)
     int lengthOfLIS(vector<int>& nums) {
 
         vector<int> vect;
@@ -7,25 +8,28 @@ public:
         
         for(int i=1; i<nums.size(); i++){
         
-            if(vect.back() < nums[i]){
+            if(vect.back() < nums[i])
                 vect.push_back(nums[i]);
-            }
             
             else if (vect.back() > nums[i]){
                 //Task is to find the element in arr just bigger or equal to nums[i]
                 int lo = 0, hi = vect.size()-1, mid;
+                
                 while(lo<hi){
+                    
                     mid = lo + (hi-lo)/2;
                     //Predicate: Update lo if nums[i]>nums[mid]
-                    //FFFFF*TTTTT*
+                    //FFFFF*TTTTT*: Find the first True according to the above Predicate
+                    
                     if(nums[i] > vect[mid])
                         lo = mid+1;
                     
                     else
                         hi = mid;
                 }
-                //Updating the elt
-                vect[hi] = nums[i];
+                //Sanity check and updating only if needed
+                if(vect[hi]>nums[i])
+                    vect[hi] = nums[i];
             }
         }
         return vect.size();
