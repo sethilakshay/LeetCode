ac789d8b5b083b2d3ad81d82e6be41af7d20fac4
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-10-24 17:05:46 -0400

    Time: 122 ms (75.09%), Space: 10.3 MB (77.05%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -4,21 +4,19 @@ public:
         int n = s.size(), res = 0;
         
         vector<int> freq(26, 0);
+        
         for(int i = 0; i<n; i++){
             
-            if(freq[s[i]-'a'] == 0){
-                freq[s[i]-'a'] = 1;
-            }
-                
+            if(freq[s[i]-'a'] != 0){
             
-            else{
                 for(int k=0; k<26; k++)
                     freq[k] = 0;
-
-                freq[s[i]-'a'] = 1;
                 
                 res++;
             }
+            
+            freq[s[i]-'a']++;
+            
         }
         return res+1;
         
