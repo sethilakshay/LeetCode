f93b0d8cf751ca23db1d23bbbb7ec3d1f00c05ab
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-10-24 17:26:09 -0400

    Time: 22 ms (74.49%), Space: 11 MB (32.02%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        //Making a vector of pairs to store number and their index
+        vector<pair<int, int>> sort_nums;
+        
+        for(int i=0; i<nums.size(); i++){
+            sort_nums.push_back({nums[i], i});
+        }
+        
+        sort(sort_nums.begin(), sort_nums.end());
+        
+        //2 pointers
+        int l = 0, r = nums.size()-1;
+        
+        while(l<r){
+            
+            if(sort_nums[l].first + sort_nums[r].first == target)
+                return {sort_nums[l].second, sort_nums[r].second};
+            
+            else if (sort_nums[l].first + sort_nums[r].first < target)
+                l++;
+            
+            else
+                r--;
+        }
+        return {-1, -1};
+        
+    }
+};
