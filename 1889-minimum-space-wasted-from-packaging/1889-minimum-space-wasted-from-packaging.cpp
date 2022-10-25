d2a4426e0f7b7c422c1550d90f01537cb3459a26
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-10-25 01:19:30 -0400

    Time: 367 ms (89.71%), Space: 110 MB (84.57%) - LeetHub

--- 1889-minimum-space-wasted-from-packaging/1889-minimum-space-wasted-from-packaging.cpp
+++ 1889-minimum-space-wasted-from-packaging/1889-minimum-space-wasted-from-packaging.cpp
@@ -1,21 +1,49 @@
 class Solution {
 public:
-    int minWastedSpace(vector<int>& A, vector<vector<int>>& boxes) {
-        sort(A.begin(), A.end());
-        long res = LONG_MAX, mod = 1e9 + 7, sumA = 0;
-        for (int a : A)
-            sumA += a;
-        for (auto& B : boxes) {
-            sort(B.begin(), B.end());
-            if (B[B.size() - 1] < A[A.size() - 1]) continue;
-            long cur = 0, i = 0, j;
-            for (int b : B) {
-                j = upper_bound(A.begin() + i, A.end(), b) - A.begin();
-                cur += b * (j - i);
-                i = j;
+    int minWastedSpace(vector<int>& packages, vector<vector<int>>& boxes) {
+        
+        int n = packages.size();
+        long packages_sum = 0, min_waste = LONG_MAX, mod = 1e9+7;
+        
+        sort(packages.begin(), packages.end());
+        int max_req = packages[n-1];
+    
+        for(auto pk: packages)
+            packages_sum += pk;
+        
+        
+        for(int i=0; i<boxes.size(); i++){
+            
+            sort(boxes[i].begin(), boxes[i].end());
+            if(boxes[i][boxes[i].size()-1] < max_req)
+                continue;
+            
+            long box_waste = 0, lo = 0, prev_lo = 0;
+            for(int j=0; j<boxes[i].size(); j++){
+                
+                if(boxes[i][j] < packages[lo])
+                    continue;
+                
+                long hi = n-1, mid;
+                while(lo<hi){
+                    mid = lo + (hi-lo+1)/2;
+                    if(packages[mid] <= boxes[i][j])
+                        lo = mid;
+                    else
+                        hi = mid-1;
+                }
+                
+                box_waste += ((lo-prev_lo+1)*boxes[i][j]);
+                
+                prev_lo = lo+1;
+                
+                if(lo == n-1)
+                    continue;
             }
-            res = min(res, cur);
+            
+            min_waste = min(min_waste, box_waste);
         }
-        return res < LONG_MAX ? (res - sumA) % mod : -1;
+        
+        return min_waste == LONG_MAX ? -1 : (min_waste-packages_sum)%mod;
     }
 };
