38454d4da8f07b8b90882dc0e6247cc87b743e6f
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-10-25 00:31:42 -0400

    Time: 30 ms (57.69%), Space: 12 MB (11.83%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
-    //Constant Space Algorithim
     vector<int> twoSum(vector<int>& nums, int target) {
         
-        //Making a vector of pairs to store number and their index
-        vector<pair<int, int>> sort_nums;
+        unordered_map<int, int> nums_map;
+        
+        for (int i=0; i<nums.size(); i++)
+            nums_map[nums[i]] = i;
         
         for(int i=0; i<nums.size(); i++){
-            sort_nums.push_back(make_pair(nums[i], i));
+            
+            if(nums_map.find(target - nums[i]) != nums_map.end() && nums_map[target - nums[i]] != i)
+                return {i, nums_map[target - nums[i]]};
         }
-        //Pair is automatically sorted on the basis of pair.first
-        sort(sort_nums.begin(), sort_nums.end());
         
-        //2 pointers
-        int l = 0, r = nums.size()-1;
-        
-        while(l<r){
-            
-            if(sort_nums[l].first + sort_nums[r].first == target)
-                return {sort_nums[l].second, sort_nums[r].second};
-            
-            else if (sort_nums[l].first + sort_nums[r].first < target)
-                l++;
-            
-            else
-                r--;
-        }
         return {-1, -1};
+            
         
     }
 };
