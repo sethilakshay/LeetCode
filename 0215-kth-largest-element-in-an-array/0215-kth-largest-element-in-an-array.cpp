6946db6ddf4a454fedb255da03d772124c420c46
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-10-31 16:30:57 -0400

    Time: 309 ms (15.24%), Space: 49.9 MB (11.55%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
-    int QuickSelect(int l, int r, vector<int>& nums, int idx){
-        int start = l, pivot = nums[r];
-        
-        for(int i=start; i<r; i++){
-            
-            if(nums[i] <= pivot){
-                
-                swap(nums[i], nums[start]);
-                start++;
-            }
-        }
-        
-        swap(nums[r], nums[start]);
-        
-        if(start > idx)
-            return QuickSelect(l, start-1, nums, idx);
-        
-        else if(start < idx)
-            return QuickSelect(start+1, r, nums, idx);
-        
-        else
-            return nums[start];
-    }
-    
     int findKthLargest(vector<int>& nums, int k) {
         
-        return QuickSelect(0, nums.size()-1, nums, nums.size()-k);
+        priority_queue<int> maxq;
+        
+        for(int num: nums)
+            maxq.push(num);
+        
+        while(k-1>0){
+            maxq.pop();
+            k--;
+        }
+        
+        return maxq.top();
     }
 };
