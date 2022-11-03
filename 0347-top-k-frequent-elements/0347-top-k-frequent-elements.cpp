a46bf7b616e68cf7ac045c59d6e2e61f3f4bf4a1
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-11-03 14:38:18 -0400

    Time: 30 ms (62.89%), Space: 13.6 MB (90.05%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        
-        struct compare{
-            bool operator()(pair<int, int>& a, pair<int, int> b){
-                return a.second < b.second;
-            }
-        };
-        
         unordered_map<int, int> hash_map;
+        
         for(int num: nums){
             hash_map[num]++;
         }
         
-        priority_queue<pair<int, int>, vector<pair<int, int>>, compare> maxq;
+        priority_queue<pair<int, int>> maxq;
         
-        for(auto it = hash_map.begin(); it != hash_map.end(); it++){
-
-            maxq.push(make_pair(it->first, it->second));
+        for(auto it: hash_map){
+            maxq.push(make_pair(it.second, it.first));
         }
         
         vector<int> res;
-        
         while(k > 0){
-            res.push_back(maxq.top().first);
+            res.push_back(maxq.top().second);
             maxq.pop();
             k--;
         }
