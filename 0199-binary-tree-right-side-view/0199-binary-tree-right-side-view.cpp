0317432e35ef46b36d948c0ee2d9307f7856b598
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-11-04 02:22:15 -0400

    Time: 3 ms (87.36%), Space: 11.9 MB (89.17%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,41 +11,25 @@
  */
 class Solution {
 public:
-    vector<int> rightSideView(TreeNode* root) {
-        vector<int> res;
-        queue<TreeNode*> q;
-        q.push(root);
-        
+    void rightView(TreeNode* root, vector<int>& res, int lvl){
         if(root == NULL){
-            return {};
+            return;
         }
         
-        res.push_back(root->val);
-        
-        while(!q.empty()){
-            
-            int n = q.size();
-            int val = INT_MIN;
-            
-            for(int i=0; i<n; i++){
-    
-                TreeNode* temp = q.front();
-                q.pop();
-                
-                if(temp->left != NULL){
-                    q.push(temp->left);
-                    val = temp->left->val;
-                }
-                if(temp->right != NULL){
-                    q.push(temp->right);
-                    val = temp->right->val;
-                }
-            }
-            
-            if(val != INT_MIN){
-                res.push_back(val);
-            }
+        if(lvl == res.size()){
+            res.push_back(root->val);
         }
+        
+        rightView(root->right, res, lvl+1);
+        rightView(root->left, res, lvl+1);
+        
+        return;
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        
+        vector<int> res;
+        rightView(root, res, 0);
+        
         return res;
     }
 };
