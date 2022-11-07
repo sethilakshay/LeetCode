d53e62e2ab65e9f601a632733821dade721ee461
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-11-07 02:06:27 -0500

    Time: 7 ms (83.36%), Space: 7.2 MB (98.53%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isAlphanumeric(char c){
         
-        return (('a'<=c && c<= 'z') || ('0'<=c && c<= '9'));
+        return (('A'<=c && c<= 'Z') || ('a'<=c && c<= 'z') || ('0'<=c && c<= '9'));
         
     }
     bool isPalindrome(string s) {
@@ -10,28 +10,17 @@ public:
         int start = 0, end = s.size()-1;
         
         while(start <= end){
-            
-            if('A' <= s[start] && s[start] <= 'Z'){
-                s[start] = s[start] - 'A' + 'a';
-            }
-            
-            if('A' <= s[end] && s[end] <= 'Z'){
-                s[end] = s[end] - 'A' + 'a';
-            }
-            
             if(!isAlphanumeric(s[start])){
                 start++;
                 continue;
             }
                 
-            
             if(!isAlphanumeric(s[end])){
                 end--;
                 continue;
             }
             
-            if(s[start] != s[end]){
-                cout<<s[start]<<s[end];
+            if(tolower(s[start]) != tolower(s[end])){
                 return false;
             }
             
