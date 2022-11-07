0b933441f12409aa01b8c4d9592d013e5087cf42
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-11-07 17:23:36 -0500

    Time: 0 ms (100.00%), Space: 6.4 MB (10.31%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,27 +3,25 @@ public:
     bool isValid(string s) {
         
         unordered_map<char, char> hash_map;
+        stack<char> stk;
         
         hash_map[')'] = '(';
         hash_map['}'] = '{';
         hash_map[']'] = '[';
         
-        stack<char> stk;
-        
         for(int i=0; i<s.size(); i++){
             
             if(hash_map.find(s[i]) != hash_map.end()){
-            
+                
                 if(stk.empty() || hash_map[s[i]] != stk.top())
                     return false;
                 else 
                     stk.pop();
             }
+            
             else
                 stk.push(s[i]);
         }
-        
-        
         return stk.empty();
         
     }
