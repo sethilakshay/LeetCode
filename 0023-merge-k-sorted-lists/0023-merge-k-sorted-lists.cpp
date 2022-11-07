db74f1c161e918a5a54b29b1d4dd57fd1569796e
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-11-06 19:16:05 -0500

    Time: 22 ms (96.81%), Space: 14.1 MB (14.67%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -11,25 +11,25 @@
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        //Initialzing a Minimum Heap and pushing elements into it
+        
         priority_queue<int, vector<int>, greater<int>> min_heap;
+        ListNode* temp;
         
         for(int i=0; i<lists.size(); i++){
-            ListNode* temp = lists[i];
+            temp = lists[i];
             
             while(temp != NULL){
                 min_heap.push(temp->val);
                 temp = temp->next;
             }
         }
+    
         
-        //Sanity Check for the base case
         if(min_heap.size() == 0)
             return NULL;
         
-        //Unpacking the heap into a LinkedList
-        ListNode* root = new ListNode(min_heap.top());
-        ListNode* temp = root;
+        ListNode* sorted = new ListNode(min_heap.top());
+        temp = sorted;
         min_heap.pop();
         
         while(!min_heap.empty()){
@@ -37,7 +37,6 @@ public:
             temp = temp->next;
             min_heap.pop();
         }
-        
-        return root;
+        return sorted;
     }
 };
