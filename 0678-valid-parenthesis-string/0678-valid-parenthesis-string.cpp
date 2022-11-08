054e2136fcb0bc07bf56537b7026d98d13883270
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-11-07 21:01:20 -0500

    Time: 3 ms (50.96%), Space: 6.1 MB (61.38%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        
+        int left_min = 0, left_max = 0;
+        
+        for(int i=0; i<s.size(); i++){
+            
+            if(s[i] == '('){
+                left_min++;
+                left_max++;
+            }
+            else if(s[i] == ')'){
+                left_min--;
+                left_max--;
+            }
+            else{
+                left_min--;
+                left_max++;
+            }
+            
+            if(left_min < 0){
+                left_min = 0;
+            }
+            if(left_max < 0){
+                return false;
+            }
+        }
+        
+        return left_min == 0 ? true : false;
+        
+    }
+};
