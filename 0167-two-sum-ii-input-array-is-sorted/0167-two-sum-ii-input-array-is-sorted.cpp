6f8a464438080d67af6b1c7ce9604d5dd197c640
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-11-07 22:58:20 -0500

    Time: 25 ms (65.92%), Space: 15.7 MB (6.11%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -5,11 +5,11 @@ public:
         int ptr1 = 0, ptr2 = numbers.size()-1;
         
         while(ptr1<ptr2){
-            
-            if(numbers[ptr1] + numbers[ptr2] == target){
+            int s = numbers[ptr1] + numbers[ptr2];
+            if(s == target){
                 return {ptr1+1, ptr2+1};
             }
-            else if(numbers[ptr1] + numbers[ptr2] > target){
+            else if(s > target){
                 ptr2--;
             }
             else{
