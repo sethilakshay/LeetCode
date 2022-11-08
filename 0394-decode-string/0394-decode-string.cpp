ddc37157f89fbb612942bd46aa2e041b43b7c6d8
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-11-07 22:39:01 -0500

    Time: 10 ms (6.04%), Space: 10.9 MB (17.01%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,44 +1,47 @@
 class Solution {
 public:
-
     string decodeString(string s) {
-        stack<char> st;
-        for(int i = 0; i < s.size(); i++){
-            if(s[i] != ']') {
-                st.push(s[i]);
+        
+        stack<char> stk;
+        string res = "";
+        
+        for(int i=0; i<s.size(); i++){
+            if(s[i] != ']'){
+                stk.push(s[i]);
             }
+
             else{
-                string curr_str = "";
-                
-                while(st.top() != '['){
-                    curr_str = st.top() + curr_str ;
-                    st.pop();
+                string temp = "";
+                while(stk.top() != '['){
+                    temp = stk.top() + temp;
+                    stk.pop();
                 }
                 
-                st.pop();   // for '['
-                string number = "";
+                stk.pop();  //To remove this [
                 
-                // for calculating number
-                
-                while(!st.empty() && isdigit(st.top())){
-                    number = st.top() + number;
-                    st.pop();
+                string num = "";
+                while(!stk.empty() && '0' <=stk.top() && stk.top()<= '9'){
+                    num = stk.top() + num;
+                    stk.pop();
                 }
-                int k_time = stoi(number);    // convert string to number
                 
-                while(k_time--){
-                    for(int p = 0; p < curr_str.size() ; p++)
-                        st.push(curr_str[p]);
+                int temp_n = stoi(num);
+                
+                while(temp_n > 0){
+                    
+                    for(int k=0; k<temp.size(); k++)
+                        stk.push(temp[k]);
+            
+                    temp_n--;
                 }
             }
         }
         
-        s = "";
-        while(!st.empty()){
-            s = st.top() + s;
-            st.pop();
+        while(!stk.empty()){
+            res = stk.top() + res;
+            stk.pop();
         }
-        return s;
         
+        return res;
     }
 };
