0ca163ee6461ba2f1f8fb1c0e71a1fa8f12370ac
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-11-07 20:39:16 -0500

    Time: 10 ms (49.04%), Space: 7.3 MB (41.94%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -3,24 +3,25 @@ public:
     int longestValidParentheses(string s) {
         
         stack<int> stk;
-        int res = 0;
-        
         stk.push(-1);
+        
+        int res = 0;
         
         for(int i=0; i<s.size(); i++){
             
-            if(s[i] == '(')
+            if(s[i] == '('){
                 stk.push(i);
-            
+            }
             else{
                 stk.pop();
                 
-                if(stk.empty())
+                if(stk.empty()){
                     stk.push(i);
+                }
                 
-                else
-                    res = max(res, i-stk.top());
+                res = max(res, i - stk.top());
             }
+            
         }
         return res;
     }
