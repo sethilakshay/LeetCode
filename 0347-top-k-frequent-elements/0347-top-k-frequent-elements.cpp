cc29bb30264dfbf0b6bdcafbd2ccddda34e969f8
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-11-08 00:06:06 -0500

    Time: 7 ms (99.92%), Space: 13.6 MB (73.49%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> hash_map;
         
-        for(int num: nums){
-            hash_map[num]++;
+        unordered_map<int, int> hash_map;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> min_heap;
+        
+        for (int i=0; i<nums.size(); i++){
+            hash_map[nums[i]]++;
         }
         
-        priority_queue<pair<int, int>> maxq;
-        
-        for(auto it = hash_map.begin(); it != hash_map.end(); it++){
-            maxq.push(make_pair(it->second, it->first));
+        for(auto it=hash_map.begin(); it!=hash_map.end(); it++){
+            
+            int num  = it->first;
+            int cnt = it->second;
+            
+            if(min_heap.size() < k)
+                min_heap.push(make_pair(cnt, num));            
+            
+            else{
+                if(cnt > min_heap.top().first){
+                    min_heap.pop();
+                    min_heap.push(make_pair(cnt, num));
+                }    
+            }
         }
         
         vector<int> res;
-        while(k > 0){
-            res.push_back(maxq.top().second);
-            maxq.pop();
-            k--;
+        while(!min_heap.empty()){
+            res.push_back(min_heap.top().second);
+            min_heap.pop();
         }
         
         return res;
