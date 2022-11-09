ee676d481562564a9230d8275d26b4891563dbbb
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-11-09 15:39:57 -0500

    Time: 46 ms (83.71%), Space: 13.3 MB (37.93%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     void dfs(int r, int c, vector<vector<char>>& grid, vector<vector<int>>& visited){
         
-        if(r<0 || r>=grid.size() ||c<0 || c>=grid[0].size() || grid[r][c] == '0' || visited[r][c] == 1){
+        if(!(0<=r && r<grid.size() && 0<=c && c<grid[0].size() && grid[r][c] == '1' && visited[r][c] == 0))
             return;
-        }
         
         visited[r][c] = 1;
+        
         dfs(r+1, c, grid, visited);
         dfs(r-1, c, grid, visited);
         dfs(r, c+1, grid, visited);
@@ -15,10 +15,8 @@ public:
     
     int numIslands(vector<vector<char>>& grid) {
         
-        if (grid.size() == 0)
-            return 0;
-
         int rows = grid.size(), cols = grid[0].size(), islands = 0;
+        
         vector<vector<int>> visited(rows, vector<int>(cols, 0));
         
         for(int i=0; i<rows; i++){
@@ -27,11 +25,10 @@ public:
                 
                 if(grid[i][j] == '1' && visited[i][j] == 0){
                     dfs(i, j, grid, visited);
-                    islands += 1;
+                    islands++;
                 }
             }
         }
-        
         return islands;
     }
 };
