968d382c720b446a3dc63e4488390c0bec6719d5
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-11-10 02:08:50 -0500

    Time: 221 ms (46.49%), Space: 58.9 MB (80.36%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    //2 Pointer Solution
     int maxArea(vector<int>& height) {
         
-        int max_area = 0, n = height.size();
+        int n = height.size(), maxA = 0;
         int i = 0, j = n-1;
         
         while(i<j){
             
-            if(height[j]<=height[i]){
-                max_area = max((j-i)*height[j], max_area);
+            if(height[j] <= height[i]){
+                
+                maxA = max(maxA, (j-i)*height[j]);
                 j--;
             }
             
             else{
-                max_area = max((j-i)*height[i], max_area);
+                maxA = max(maxA, (j-i)*height[i]);
                 i++;
             }
         }
-        return max_area;
+        return maxA;
     }
 };
