b76487ecae0cb58411779fbdb458f85dfecdaa7a
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-11-10 01:30:48 -0500

    Time: 97 ms (93.35%), Space: 41.5 MB (48.42%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -2,22 +2,21 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         
-        unordered_map<int, int> hash_map;
+        unordered_map <int, int> sum_map;   //Using maps instead of 2-pointers since we can have -ve numbers
+        
         int i, n = nums.size(), cum_sum = 0, res = 0;
         
-        hash_map[0] = 1;
+        sum_map[0] = 1;     //Initializing sum=0 to 1 because we can also have 1 empty array
         
         for(i=0; i<n; i++){
-            
             cum_sum += nums[i];
             
-            if(hash_map.find(cum_sum-k) != hash_map.end()){
-                
-                res += hash_map[cum_sum-k];
-            }
-            hash_map[cum_sum]++;
+            if(sum_map.find(cum_sum - k) != sum_map.end())
+                res += sum_map[cum_sum - k];
+            
+            sum_map[cum_sum]++;
+            
         }
         return res;
-        
     }
 };
