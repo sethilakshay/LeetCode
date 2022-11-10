c0e881ee6b095139dce8d2b176150c871564a603
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-11-09 21:06:14 -0500

    Time: 37 ms (67.06%), Space: 19.2 MB (44.49%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,40 +1,35 @@
 class Solution {
 public:
-    //Worst case TC: O(N^2)
     string longestPalindrome(string s) {
-        int n = s.size(), maxC = 0;
-        string res;
         
-        for(int i=0; i<n; i++){ 
+        int n = s.size(), maxC = 0;
+        string res = "";
+        
+        for(int i=0; i<n; i++){
             
-            //For odd length strings
             int start = i, end = i;
-            while((start>=0) && (end<n) && (s[start]==s[end])){
+            while(start >= 0 && end <n && s[start] == s[end]){
+                int strLen = end-start+1;
                 
-                int str_len = end-start+1;
-                if(str_len > maxC){
-                        maxC = str_len;
-                        res = s.substr(start, str_len);
+                if(strLen > maxC){
+                    maxC = strLen;
+                    res = s.substr(start, strLen);
                 }
-                    
                 start--;
                 end++;
             }
             
-            //For even length strings
             start = i, end = i+1;
-            while((start>=0) && (end<n) && (s[start]==s[end])){
+            while(start >= 0 && end <n && s[start] == s[end]){
+                int strLen = end-start+1;
                 
-                int str_len = end-start+1;
-                if(str_len > maxC){
-                        maxC = str_len;
-                        res = s.substr(start, str_len);
+                if(strLen > maxC){
+                    maxC = strLen;
+                    res = s.substr(start, strLen);
                 }
-                    
                 start--;
                 end++;
             }
-            
         }
         return res;
     }
