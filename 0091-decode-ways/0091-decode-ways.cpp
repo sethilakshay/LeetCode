e443d33bf7b8c3c757483dea226e1b67f1fd8300
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-11-10 10:16:12 -0500

    Time: 0 ms (100.00%), Space: 6.3 MB (45.89%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     int numDecodings(string s) {
         int n = s.size();
-        
+        //Shifting DP vector by 1 position to the right
         vector<int> dp(n+1, 0);
         
+        //Initializing values at edge cases
         dp[0] = 1;
-        dp[1] = s[0] == '0' ? 0:1;
+        dp[1] = s[0]=='0' ? 0 : 1;
         
         for(int i=1; i<n; i++){
-            
-            if(s[i] != '0'){
+            if(s[i] != '0')
                 dp[i+1] += dp[i];
-            }
             
-            if(s[i-1] == '1' || (s[i-1] == '2' && s[i]<='6')){
-                dp[i+1] += dp[i-1];
-            }
+            if(s[i-1] == '1' || (s[i-1] == '2' && s[i]<='6'))
+                dp[i+1] += dp[i-1]; 
         }
         return dp[n];
     }
