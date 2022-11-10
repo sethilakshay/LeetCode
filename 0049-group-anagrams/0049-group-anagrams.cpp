148461ff2a1584adbbe2949f82c5a763526305d2
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-11-10 00:19:25 -0500

    Time: 60 ms (73.82%), Space: 18.3 MB (94.39%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        unordered_map<string, int> hash_map;
+        vector<vector<string>> res;
+        
+        for(int i=0; i<strs.size(); i++){
+            
+            string temp = strs[i];
+            
+            sort(temp.begin(), temp.end());
+            
+            if(hash_map.find(temp) == hash_map.end()){
+                
+                res.push_back(vector<string>());
+                hash_map[temp] = res.size()-1;
+            }
+            
+            res[hash_map[temp]].push_back(strs[i]);
+            
+        }
+        return res;
+    }
+};
