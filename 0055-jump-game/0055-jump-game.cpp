0d41058fcb399be0c6912daacc35636393c6f870
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-11-11 15:33:30 -0500

    Time: 1706 ms (9.44%), Space: 50.6 MB (15.08%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        vector<int> dp(n, false);
+        
+        dp[n-1] = true;
+        
+        for(int i = n-2; i>= 0; i--){
+            
+            if(i + nums[i] >= n-1){
+                dp[i] = true;
+            }
+            else{
+                
+                for(int j = 1; j <= nums[i]; j++){
+                    if(dp[i+j]){
+                        dp[i] = true;
+                        break;
+                    }                
+                }
+            }
+        }
+        return dp[0];
+    }
+};
