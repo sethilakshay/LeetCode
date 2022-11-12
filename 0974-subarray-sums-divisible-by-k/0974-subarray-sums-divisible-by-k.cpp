99e96322bc42ab69bcc6dffcea9f544aa9888f2d
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-11-11 23:20:09 -0500

    Time: 196 ms (6.13%), Space: 31.6 MB (73.89%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
+        //Key = Postive Remainder
+        //Value = Number of times the positive remainder comes
+        unordered_map<int, int> sum_map;
         
-        unordered_map<int, int> hash_map;
+        int i, j, n = nums.size(), cum_sum = 0, rem, res = 0;
+        sum_map[0] = 1; //Initializing rem=0 to 1 because of empty array
         
-        int res = 0, cum_sum = 0, rem;
-    
-        hash_map[0] = 1;
-        for(int i=0; i<nums.size(); i++){
+        for(i=0; i<n; i++){
+            cum_sum += nums[i];     //Updating cumulative sum
             
-            cum_sum += nums[i];
+            //Calculating remainder for cum_sum and checking if another sub-array with same remainder exists
+            //However, if cum_sum is negative, then remainders will be negative
+            //To get +ve remainders, simply add k to the -ve remainder
             
-            rem = cum_sum%k;
-            if(rem<0)
+            rem = cum_sum%k;    //This remainder can be +ve or -ve depending on cum_sum            
+            if(rem<0)           //Adjusting for negative remainders
                 rem += k;
             
-            if(hash_map.find(rem) != hash_map.end())
-                res += hash_map[rem];
+            if(sum_map.find(rem) != sum_map.end())
+                res += sum_map[rem];
             
-            hash_map[rem]++;
+            sum_map[rem]++;
         }
         return res;
     }
