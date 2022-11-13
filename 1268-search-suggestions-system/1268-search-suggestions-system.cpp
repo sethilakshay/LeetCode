b7f4ab38d0ff3cf5221946ed3eca2476bc7f828c
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-11-12 19:39:59 -0500

    Time: 335 ms (36.85%), Space: 70 MB (56.79%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,46 +1,70 @@
-struct TrieNode {
-    vector<TrieNode*> child{vector<TrieNode*>(26, nullptr)};;
-    string* word{};
-};
-void insert(TrieNode* root, string& word) {
-    for (char c : word) {
-        c -= 'a';
-        if (root->child[c] == nullptr) root->child[c] = new TrieNode();
-        root = root->child[c];
-    }
-    root->word = &word;
-}
-void dfs(TrieNode* root, int limit, vector<string>& result) {
-    if (root == nullptr) return;
-    if (result.size() == limit) return;
-    if (root->word != nullptr) result.push_back(*root->word);
-    for (int i = 0; i < 26; ++i)
-        dfs(root->child[i], limit, result);
-}
-vector<string> getWords(TrieNode* root, int limit) {
-    vector<string> result;
-    dfs(root, limit, result);
-    return result;
-}
-
 class Solution {
 public:
-    vector<vector<string>> suggestedProducts(vector<string> &products, string searchWord) {
-        TrieNode* root = new TrieNode();
-        for (string &product : products) insert(root, product);
-
-        vector<vector<string>> ans;
-        TrieNode* curr = root;
-        for (char &c : searchWord) {
-            c -= 'a';
-            if (curr != nullptr && curr->child[c] != nullptr) {
-                curr = curr->child[c];
-                ans.push_back(getWords(curr, 3));
-            } else {
-                curr = nullptr;
-                ans.emplace_back();
+    struct TrieNode{
+        TrieNode* child[26] = {NULL};
+        string word = "";
+    };
+    
+    TrieNode* root = new TrieNode();
+    
+    void buildTree(vector<string>& products){
+        
+        for(int i=0; i<products.size(); i++){
+            TrieNode* temp = root;
+            
+            for(int j=0; j<products[i].size(); j++){
+                
+                int idx = products[i][j] - 'a';
+                if(temp->child[idx] == NULL){
+                    temp->child[idx] = new TrieNode();
+                }
+                temp = temp->child[idx];
+            }
+            temp->word = products[i];
+        }
+    }
+    
+    void DFS(TrieNode* root, int limit, vector<string>& res){
+        if(root == NULL)
+            return;
+        
+        if(res.size() == limit)
+            return;
+        
+        if(root->word.size() > 0){
+            res.push_back(root->word);
+        }
+        
+        for(int i=0; i<26; i++){
+            DFS(root->child[i], limit, res);
+        }
+        return;
+    }
+    
+    vector<string> findWords(TrieNode* root, int limit){
+        vector<string> res;
+        DFS(root, limit, res);
+        return res;
+    }
+    
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        buildTree(products);
+        
+        vector<vector<string>> res;
+        
+        TrieNode* temp = root;
+        
+        for(char c: searchWord){
+            int idx = c - 'a';
+            if(temp!= NULL && temp->child[idx] != NULL){
+                temp = temp->child[idx];
+                res.push_back(findWords(temp, 3));
+            }
+            else{
+                temp = NULL;
+                res.push_back({});
             }
         }
-        return ans;
+        return res;
     }
 };
