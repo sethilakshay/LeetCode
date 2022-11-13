312da5805bd0d686a88c870869369e73ae5da7ff
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-11-12 21:19:23 -0500

    Time: 5 ms (42.15%), Space: 6.3 MB (36.46%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -1,30 +1,36 @@
+class compare{
+public:
+    bool operator() (pair<char, int>& a, pair<char, int>& b){
+        return a.second<b.second;
+    }
+};
 class Solution {
 public:
     string reorganizeString(string s) {
-        unordered_map<char, int> map;
         
+        unordered_map<char, int> map;
         for(char c: s){
             map[c]++;
         }
         
-        priority_queue<pair<int, char>> max_heap;
+        priority_queue<pair<char, int>, vector<pair<char, int>>, compare> max_heap;
         
         for(auto it = map.begin(); it != map.end(); it++){
-            max_heap.push({it->second, it->first});
+            max_heap.push({it->first, it->second});
         }
         
         string new_word = "";
-        pair<int, char> curr;
-        pair<int, char> prev;
+        pair<char, int> curr;
+        pair<char, int> prev;
         
         while(!max_heap.empty()){
             curr = max_heap.top();
             max_heap.pop();
             
-            new_word += curr.second;
-            curr.first--;
+            new_word += curr.first;
+            curr.second--;
             
-            if(prev.first != 0)
+            if(prev.second != 0)
                 max_heap.push(prev);
             
             prev = curr;
