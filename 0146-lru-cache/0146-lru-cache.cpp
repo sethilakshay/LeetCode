1e43da6c353af88984919e093e007834cd90f729
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-11-15 09:50:02 -0500

    Time: 473 ms (77.50%), Space: 174.1 MB (29.89%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,41 +1,45 @@
 class LRUCache {
 public:
-    unordered_map<int, list<pair<int, int>>::iterator> reference_map; 
+    unordered_map<int, list<pair<int, int>>::iterator> cache_map;
     list<pair<int, int>> cache;
     int capacity;
+    
     LRUCache(int capacity) {
         this->capacity = capacity;
+        cache.clear();
     }
     
     int get(int key) {
-        if(reference_map.find(key)==reference_map.end()){
-            return-1;
-        }
-        int temp=reference_map[key]->second;
-        cache.erase(reference_map[key]);
-        cache.push_front({key,temp});
-        reference_map[key]=cache.begin();
+        if(cache_map.find(key) == cache_map.end())
+           return -1;
+           
+        int temp = cache_map[key]->second;
+        cache.erase(cache_map[key]);
+        cache.push_front({key, temp});
+        cache_map[key] = cache.begin();
         return temp;
     }
     
-    void put(int key, int value){
-        if(reference_map.find(key)!=reference_map.end()){
-            cache.erase(reference_map[key]);
-            cache.push_front({key,value});
-            reference_map[key]=cache.begin();
+    void put(int key, int value) {
+        
+        if(cache_map.find(key) != cache_map.end()){
+            cache.erase(cache_map[key]);
+            cache.push_front({key, value});
+            cache_map[key] = cache.begin();
         }
+        
+        else if(cache.size() < capacity){
+            cache.push_front({key, value});
+            cache_map[key] = cache.begin();
+        }
+        
         else{
-            if(cache.size()<capacity){
-                cache.push_front({key,value});
-                reference_map[key]=cache.begin();
-            }
-            else{
-                int evict=cache.back().first;
-                reference_map.erase(evict);
-                cache.pop_back();
-                cache.push_front({key,value});
-                reference_map[key]=cache.begin();
-            }
+            int evict = cache.back().first;
+            cache.pop_back();
+            cache_map.erase(evict);
+            
+            cache.push_front({key, value});
+            cache_map[key] = cache.begin();
         }
     }
 };
