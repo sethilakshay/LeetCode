2b8ed071c3c40685861d34069852c6e1c81be1bd
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-11-28 02:07:02 -0500

    Time: 4 ms (33.43%), Space: 6.3 MB (80.10%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,23 +2,28 @@ class Solution {
 public:
     bool isValid(string s) {
         
-        unordered_map<char, char> map;
-        
-        map[')'] = '(';
-        map['}'] = '{';
-        map[']'] = '[';
+        unordered_map<char, char> bracket_map;
+        bracket_map[')'] = '(';
+        bracket_map['}'] = '{';
+        bracket_map[']'] = '[';
         
         stack<char> stk;
         
         for(int i=0; i<s.size(); i++){
             
-            if(map.find(s[i]) != map.end() && !stk.empty() && stk.top() == map[s[i]]){
-                stk.pop();
+            if(bracket_map.find(s[i]) != bracket_map.end()){
+                
+                if(!stk.empty() && stk.top() == bracket_map[s[i]])
+                    stk.pop();
+                else
+                    return false;
             }
+            
             else{
                 stk.push(s[i]);
             }
         }
-        return stk.empty();
+        
+        return stk.empty() ? true : false;
     }
 };
