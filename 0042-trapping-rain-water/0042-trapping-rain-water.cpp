46e63791d91b6dd7e6a82f8422d5959238531921
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-11-28 01:27:15 -0500

    Time: 40 ms (35.89%), Space: 20.5 MB (25.98%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    //TC: O(N)
     int trap(vector<int>& height) {
-        int n = height.size(), res = 0, max_h = INT_MIN;
+        int n = height.size(), res = 0;
+        vector<int> left(n);
+        vector<int> right(n);
         
-        vector<int> left_h(n, 0);   //Storing the maximum encountered height on the left side
-        vector<int> right_h(n, 0);  //Storing the maximum encountered height on the right side
-        
+        int maxh = INT_MIN;
         for(int i=0; i<n; i++){
-            max_h = max(max_h, height[i]);
-            left_h[i] = max_h;
+            
+            if(height[i] > maxh){
+                maxh = height[i];
+            }
+            left[i] = maxh;
         }
         
-        max_h = INT_MIN;    //Very important here to re-initialize
+        maxh = INT_MIN;
         for(int i=n-1; i>=0; i--){
-            max_h = max(max_h, height[i]);
-            right_h[i] = max_h;
+            
+            if(height[i] > maxh){
+                maxh = height[i];
+            }
+            right[i] = maxh;
         }
         
         for(int i=0; i<n; i++){
-            res += min(left_h[i], right_h[i]) - height[i];
+            res += min(left[i], right[i]) - height[i];
         }
         
         return res;
