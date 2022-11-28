0de4dabf4767fca120067175b9e7c61a854053e8
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-11-28 08:02:55 -0500

    Time: 98 ms (92.04%), Space: 41.6 MB (48.99%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -6,13 +6,13 @@ public:
         int res = 0, run_sum = 0;
         
         hash_map[0] = 1;
-        for(int i = 0; i<nums.size(); i++){
+        
+        for(int i=0; i<nums.size(); i++){
             run_sum += nums[i];
             
             if(hash_map.find(run_sum-k) != hash_map.end()){
                 res += hash_map[run_sum-k];
             }
-            
             hash_map[run_sum]++;
         }
         return res;
