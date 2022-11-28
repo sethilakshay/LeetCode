3e5586a68689617a847fee65b65e6a2e246deef7
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-11-28 01:07:46 -0500

    Time: 13 ms (91.70%), Space: 12.1 MB (7.89%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -7,10 +7,11 @@ public:
         for(int i=0; i<nums.size(); i++){
             hash_map[nums[i]] = i;
         }
-            
+        
         for(int i=0; i<nums.size(); i++){
-            if(hash_map.find(target - nums[i]) != hash_map.end() && hash_map[target - nums[i]] != i){
-                return {i, hash_map[target - nums[i]]};
+            
+            if(hash_map.find(target-nums[i]) != hash_map.end() && hash_map[target-nums[i]] != i){
+                return {i, hash_map[target-nums[i]]};
             }
         }
         return {-1, -1};
