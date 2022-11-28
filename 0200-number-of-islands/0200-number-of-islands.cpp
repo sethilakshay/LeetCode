dbb0cff581962f1f8206492f19cec2550cd3c069
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-11-28 01:42:19 -0500

    Time: 115 ms (15.89%), Space: 13.5 MB (31.80%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,49 +1,36 @@
 class Solution {
 public:
-    void bfs(int r, int c, vector<vector<char>>& grid, vector<vector<bool>>& visited){
-        int row = grid.size(), col = grid[0].size();
-        vector<pair<int, int>> dirs = {{0,1}, {0,-1}, {1,0}, {-1, 0}};
-        
-        queue<pair<int, int>> q;
-        q.push({r, c});
-        visited[r][c] = true;
-        
-        while(!q.empty()){
-            
-            int mod_r = q.front().first;
-            int mod_c = q.front().second;
-            q.pop();
-            
-            for(int i=0; i<dirs.size(); i++){
-                int new_r = mod_r + dirs[i].first;
-                int new_c = mod_c + dirs[i].second;
-                
-                if(0<=new_r && new_r<row && 0<=new_c && new_c<col){
-                    
-                    if(grid[new_r][new_c] == '1' && visited[new_r][new_c] == false){
-                        visited[new_r][new_c] = true;
-                        q.push({new_r, new_c});
-                    }
-                }
-                
-            }
-            
-        }
-        
-    }
     int numIslands(vector<vector<char>>& grid) {
         
         int row = grid.size(), col = grid[0].size(), res = 0;
+        vector<pair<int, int>> dirs = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+        
+        queue<pair<int, int>> q;
         vector<vector<bool>> visited(row, vector<bool>(col, false));
         
         for(int i=0; i<row; i++){
-            
             for(int j=0; j<col; j++){
                 
                 if(grid[i][j] == '1' && visited[i][j] == false){
+                    q.push(make_pair(i, j));
+                    res += 1;
                     
-                    bfs(i, j, grid, visited);
-                    res++;
+                    while(!q.empty()){
+                        pair curr = q.front();
+                        q.pop();
+                        
+                        for(int k=0; k<dirs.size(); k++){
+                            int mod_r = curr.first + dirs[k].first;
+                            int mod_c = curr.second + dirs[k].second;
+                            
+                            if(0 <= mod_r && mod_r < row && 0 <= mod_c && mod_c < col){
+                                if(grid[mod_r][mod_c] == '1' && visited[mod_r][mod_c] == false){
+                                    visited[mod_r][mod_c] = true;
+                                    q.push({mod_r, mod_c});
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
