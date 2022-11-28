c527741fa6f8d93381aa2316c0424e957dfa0258
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-11-28 05:05:15 -0500

    Time: 0 ms (100.00%), Space: 11.1 MB (98.49%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
+    //Dynamic Programming Solution
     int maxProfit(vector<int>& prices) {
+        int n = prices.size(), i;
         
-        int n = prices.size();
+        if(n<=1) return 0;
         
-        if(n<=1)
-            return 0;
+        //Intitializing two vectors to track max profit
+        vector<int> dp_buy(n, 0); //max profit when last transaction was buy
+        vector<int> dp_sell(n, 0); //max profit when last transaction was sell
         
-        vector<int> dp_buy(n, 0);
-        vector<int> dp_sell(n, 0);
-        
+        //Defining Edge cases
         dp_buy[0] = -prices[0];
         dp_sell[0] = 0;
         
         dp_buy[1] = max(dp_buy[0], -prices[1]);
         dp_sell[1] = max(dp_sell[0], dp_buy[0] + prices[1]);
         
-        for(int i=2; i<n; i++){
-            dp_buy[i] = max(dp_buy[i-1], dp_sell[i-2] - prices[i]);
-            dp_sell[i] = max(dp_sell[i-1], dp_buy[i-1]+prices[i]);
+        for(i=2 ; i<n; i++){
+            dp_buy[i] = max(dp_buy[i-1], dp_sell[i-2] - prices[i]); //dp_sell[i-2] due to cooldown 
+            dp_sell[i] = max(dp_sell[i-1], dp_buy[i-1] + prices[i]);
         }
+        
         return dp_sell[n-1];
     }
 };
