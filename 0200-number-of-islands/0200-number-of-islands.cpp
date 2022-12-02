aa9e013111b27658dd45dfd857c91812dde02ff7
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-02 01:17:02 -0500

    Time: 76 ms (63.85%), Space: 18.7 MB (18.23%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,32 +1,47 @@
 class Solution {
 public:
-    void dfs(int r, int c, vector<vector<bool>>& visited, vector<vector<char>>& grid){
+    void bfs(int r, int c, vector<vector<bool>>& visited, vector<vector<char>>& grid){
         int row = grid.size(), col = grid[0].size();
         
-        if(!(r>=0 && r<row && c>=0 && c<col && visited[r][c] == false && grid[r][c] == '1')){
-            return;
-        }
+        vector<pair<int, int>> dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        
+        queue<pair<int, int>> grid_q;
+        grid_q.push(make_pair(r, c));
         
         visited[r][c] = true;
         
-        dfs(r+1, c, visited, grid);
-        dfs(r-1, c, visited, grid);
-        dfs(r, c+1, visited, grid);
-        dfs(r, c-1, visited, grid);
-        
-        return;
+        while(!grid_q.empty()){
+            pair curr = grid_q.front();
+            grid_q.pop();
+            
+            for(int i=0; i<dirs.size(); i++){
+                int mod_r = curr.first + dirs[i].first;
+                int mod_c = curr.second + dirs[i].second;
+                
+                if(mod_r >= 0 && mod_r < row && mod_c >= 0 && mod_c < col){
+                    
+                    if(grid[mod_r][mod_c] == '1' && visited[mod_r][mod_c] == false){
+                        visited[mod_r][mod_c] = true;
+                        grid_q.push(make_pair(mod_r, mod_c));
+                    }
+                }
+            }
+        }
     }
+    
     int numIslands(vector<vector<char>>& grid) {
+        
         int row = grid.size(), col = grid[0].size(), res = 0;
-        vector<vector<bool>> visited(row, vector<bool>(col, false));
+        vector<vector<bool>> visited(row, vector<bool> (col, false));
         
         for(int i=0; i<row; i++){
             
             for(int j=0; j<col; j++){
                 
                 if(grid[i][j] == '1' && visited[i][j] == false){
+                    
                     res++;
-                    dfs(i, j, visited, grid);
+                    bfs(i, j, visited, grid);
                 }
             }
         }
