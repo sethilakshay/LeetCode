eee956357cba399ad96ac27ca74f51fc483e1b18
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-07 02:44:56 -0500

    Time: 97 ms (20.92%), Space: 19.1 MB (70.40%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,10 +3,9 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         
         sort(intervals.begin(), intervals.end());
-        vector<vector<int>> res;
         
+        vector<vector<int>> res;
         vector<int> prev = intervals[0];
-        //vector<int> curr;
         
         for(int i=1; i<intervals.size(); i++){
             
