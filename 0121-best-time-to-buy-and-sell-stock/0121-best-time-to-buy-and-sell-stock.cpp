7249fddb5eb44ca7c48ed2b937dd06530a5c59cf
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-08 03:36:38 -0500

    Time: 131 ms (97.00%), Space: 93.4 MB (51.70%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -6,10 +6,7 @@ public:
         
         for(int i=0; i<prices.size(); i++){
             
-            if(buy > prices[i]){
-                buy = prices[i];
-            }
-            
+            buy = min(buy, prices[i]);
             res = max(res, prices[i] - buy);
         }
         return res;
