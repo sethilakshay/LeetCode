39f44ef58e954118a0727330e114ffd09dc10d93
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-17 12:02:22 -0500

    Time: 36 ms (57.19%), Space: 12.1 MB (62.38%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -2,30 +2,32 @@ class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
         
-        int n = intervals.size(), i = 0, j = 0, res = 0, cnt = 0;
+        vector<int> start;
+        vector<int> end;
         
-        vector<int> start(n);
-        vector<int> end(n);
-        
-        for(int i=0; i<n; i++){
-            start[i] = intervals[i][0];
-            end[i] = intervals[i][1];
+        for(int i = 0; i<intervals.size(); i++){
+            start.push_back(intervals[i][0]);
+            end.push_back(intervals[i][1]);
         }
         
         sort(start.begin(), start.end());
         sort(end.begin(), end.end());
         
-        while(i<n && j<=i){
+        int res = 0, cnt = 0, i = 0, j = 0;
+        
+        while(i<intervals.size() && j<=i){
+            
             if(start[i] < end[j]){
                 cnt++;
                 i++;
-            }
-            else{
+            } else{
                 cnt--;
                 j++;
             }
-            res = max(res, cnt);
+            
+            res = max(cnt, res);
         }
+        
         return res;
     }
 };
