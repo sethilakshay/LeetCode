480049be4790ba55119f5505ade4a2436e0813f7
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-18 23:44:27 -0500

    Time: 4 ms (34.97%), Space: 10.9 MB (15.24%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     string decodeString(string s) {
-        
         stack<char> stk;
+        string res = "";
+        
         for(int i=0; i<s.size(); i++){
-            
             if(s[i] != ']'){
                 stk.push(s[i]);
             }
-
             else{
-                string curr_str = "";
+                string curr = "";
                 while(stk.top() != '['){
-                    curr_str = stk.top() + curr_str;
+                    curr = stk.top() + curr;
                     stk.pop();
                 }
                 
-                stk.pop();  //To remove this [
+                stk.pop(); // to remove [
+                
                 string num = "";
                 
                 while(!stk.empty() && isdigit(stk.top())){
@@ -24,21 +24,21 @@ public:
                     stk.pop();
                 }
                 
-                int repeat = stoi(num);
-                while(repeat--){
-                    
-                    for(int k=0; k<curr_str.size(); k++)
-                        stk.push(curr_str[k]);
+                int n = stoi(num);
+                
+                while(n > 0){
+                    for(char c: curr){
+                        stk.push(c);
+                    }
+                    n--;
                 }
             }
         }
         
-        string res = "";
         while(!stk.empty()){
             res = stk.top() + res;
             stk.pop();
         }
-        
         return res;
     }
 };
