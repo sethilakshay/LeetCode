1e993507760d2f909c2bf5f9e611b39b383cc0cf
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-19 04:52:22 -0500

    Time: 1188 ms (26.12%), Space: 8.2 MB (7.45%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
-
-    bool find(vector<vector<char>>& board, string& word, int indx, vector<vector<bool>>& visited, int i, int j){ 
-        if(indx == word.size()){
+    bool dfs(vector<vector<char>>& board, string word, vector<vector<bool>>& visited, int r, int c, int idx){
+        int row = board.size(), col = board[0].size();
+        if(idx == word.size()){
             return true;
         }
-        int row = board.size(), col = board[0].size();
         
-        if (i<0 || j<0 || i>=row || j>=col || word[indx]!=board[i][j] || visited[i][j]==true){
+        if(!(r>=0 && r<row && c>=0 && c<col && visited[r][c]==false && board[r][c]==word[idx])){
             return false;
         }
+
+        visited[r][c] = true;
+        bool res;
         
-        bool res=false;
+        res = res || dfs(board, word, visited, r+1, c, idx+1);
+        res = res || dfs(board, word, visited, r-1, c, idx+1);
+        res = res || dfs(board, word, visited, r, c+1, idx+1);
+        res = res || dfs(board, word, visited, r, c-1, idx+1);
         
-        visited[i][j]=true;
-        
-        res = res || find(board, word, indx+1, visited, i+1, j);
-        res = res || find(board, word, indx+1, visited, i-1, j);
-        res = res || find(board, word, indx+1, visited, i, j+1);
-        res = res || find(board, word, indx+1, visited, i, j-1);
-        
-        visited[i][j]=false;
+        visited[r][c] = false;
         return res;
     }
-    bool exist(vector<vector<char>>& board, string& word){
-        int row = board.size(), col = board[0].size();
-        vector<vector<bool>> visited(row, vector<bool>(col, false));
+    bool exist(vector<vector<char>>& board, string word) {
         
-        for (int i=0; i<row; i++){
-            for (int j=0; j<col; j++){
-                bool res = find(board, word, 0, visited, i, j);
-                if (res){
+        int row = board.size(), col = board[0].size();
+        vector<vector<bool>> visited(row, vector<bool> (col, false));
+        
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                
+                if(dfs(board, word, visited, i, j, 0)){
                     return true;
                 }
             }
