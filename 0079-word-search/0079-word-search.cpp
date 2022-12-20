4f07d12f4b900c89cc048882394f7fba1d4d9798
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-20 16:09:46 -0500

    Time: 762 ms (38.30%), Space: 8.1 MB (16.09%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
-    bool dfs(vector<vector<char>>& board, string& word, vector<vector<bool>>& visited, int r, int c, int idx){
-        int row = board.size(), col = board[0].size();
+    bool wordChck(vector<vector<char>>& board, vector<vector<bool>>& visited, string& word, int idx, int r, int c){
+        // Base Case
         if(idx == word.size()){
             return true;
         }
         
+        // Out of bounds, memoization and word check
+        int row = board.size(), col = board[0].size();
         if(!(r>=0 && r<row && c>=0 && c<col && visited[r][c]==false && board[r][c]==word[idx])){
             return false;
         }
-
-        visited[r][c] = true;
-        bool res = false;
         
-        res = res || dfs(board, word, visited, r+1, c, idx+1);
-        res = res || dfs(board, word, visited, r-1, c, idx+1);
-        res = res || dfs(board, word, visited, r, c+1, idx+1);
-        res = res || dfs(board, word, visited, r, c-1, idx+1);
+        visited[r][c] = true;
+        bool res;
+        
+        res = res || wordChck(board, visited, word, idx+1, r+1, c);
+        res = res || wordChck(board, visited, word, idx+1, r-1, c);
+        res = res || wordChck(board, visited, word, idx+1, r, c+1);
+        res = res || wordChck(board, visited, word, idx+1, r, c-1);
         
         visited[r][c] = false;
         return res;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        
         int row = board.size(), col = board[0].size();
         vector<vector<bool>> visited(row, vector<bool> (col, false));
         
         for(int i=0; i<row; i++){
             for(int j=0; j<col; j++){
                 
-                if(dfs(board, word, visited, i, j, 0)){
+                if(wordChck(board, visited, word, 0, i, j)){
                     return true;
                 }
             }
