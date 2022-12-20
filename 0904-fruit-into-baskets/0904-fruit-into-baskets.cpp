69ceded8a885c99978c524bf4855d8b99c6652fd
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-20 00:03:56 -0500

    Time: 297 ms (60.64%), Space: 66.2 MB (95.04%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -5,14 +5,14 @@ public:
         int idx1 = 0, idx2 = -1, res = 0, count = 0;
         
         for(int i=0; i<fruits.size(); i++){
-
+            res = max(res, count);
             count++;
             if(fruits[i] == fruits[idx1]){
                 idx1 = i;
             } else if(idx2 == -1 || fruits[i] == fruits[idx2]){
                 idx2 = i;
             } else {
-                res = max(res, count-1);
+                
                 count = abs(idx1 - idx2) + 1;
                 idx1 = i-1;
                 idx2 = i;
