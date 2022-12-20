1a5c4b8dc7230279d0357cc949e8057b28c53b34
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-20 16:29:15 -0500

    Time: 5 ms (12.21%), Space: 6.2 MB (29.32%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -6,13 +6,13 @@ public:
             return n;
         
         vector<int> dp(n, 0);
-        dp[0] = 1;
-        dp[1] = 2;
+        dp[n-1] = 1;
+        dp[n-2] = 2;
         
-        for(int i=2; i<n ; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+        for(int i=n-3; i>=0 ; i--){
+            dp[i] = dp[i+1] + dp[i+2];
         }
-        return dp[n-1];
+        return dp[0];
         
     }
 };
