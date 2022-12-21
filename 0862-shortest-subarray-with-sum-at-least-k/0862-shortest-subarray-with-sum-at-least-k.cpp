032a91c4ef0e7e4653629f7ed944d9d7db1cdb30
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-21 18:55:07 -0500

    Time: 289 ms (82.71%), Space: 104.9 MB (76.54%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.cpp
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
     int shortestSubarray(vector<int>& nums, int k) {
-        
         int n = nums.size(), res = INT_MAX;
-        deque<int> dq;
         
-        vector<long long> pref_sum(n);  // To store the prefix sum
+        deque<int> dq_idx;
+        vector<long long> pref_sum(n, 0);
         pref_sum[0] = nums[0];
         
         for(int i=0; i<n; i++){
             
-            if(i>0){
-                pref_sum[i] = nums[i] + pref_sum[i-1];  // Computing the prefix sum
+            if(i > 0){
+                pref_sum[i] = pref_sum[i-1] + nums[i];
             }
             
             if(pref_sum[i] >= k){
-                res = min(res, i+1);    // Finding result when condition gets satisfied
+                res = min(res, i+1);
             }
             
-            // Similar technique employed like the sliding window
-            while(!dq.empty() && pref_sum[i] - pref_sum[dq.front()] >= k){
-                res = min(res, i - dq.front());
-                dq.pop_front();
+            while(!dq_idx.empty() && pref_sum[i] - pref_sum[dq_idx.front()] >= k){
+                res = min(res, i - dq_idx.front());
+                dq_idx.pop_front();
             }
             
-            // To ensure that the queue remains monotonically increasing
-            while(!dq.empty() && pref_sum[dq.back()] >= pref_sum[i]){
-                dq.pop_back();
+            while(!dq_idx.empty() && pref_sum[dq_idx.back()] > pref_sum[i]){
+                dq_idx.pop_back();
             }
             
-            dq.push_back(i);
+            dq_idx.push_back(i);
         }
         
         return res == INT_MAX ? -1 : res;
