1bee2669984fa24d5fdd35dca1a32b730a210740
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-20 22:04:34 -0500

    Time: 669 ms (25.68%), Space: 104.9 MB (76.53%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.cpp
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     int shortestSubarray(vector<int>& nums, int k) {
-        int n = nums.size(), res = INT_MAX;
         
+        int n = nums.size(), res = INT_MAX;
         deque<int> dq;
-        vector<long long> pref_sum(n); // Prefix array
+        
+        vector<long long> pref_sum(n);
         pref_sum[0] = nums[0];
         
-        for(int i=0; i<n; ++i) {
+        for(int i=0; i<n; i++){
             
-            if(i > 0) {
-                pref_sum[i] = nums[i] + pref_sum[i-1]; // Prefix Sum
+            if(i>0){
+                pref_sum[i] = nums[i] + pref_sum[i-1];
             }
             
-            if(pref_sum[i] >= k) {
-                res = min(res, i+1); // The subarray 0'th to i'th index can be an answer
+            if(pref_sum[i] >= k){
+                res = min(res, i+1);
             }
             
-            while(!dq.empty() && pref_sum[i] - pref_sum[dq.front()] >= k) {
+            while(!dq.empty() && pref_sum[i] - pref_sum[dq.front()] >= k){
                 res = min(res, i - dq.front());
+                    
                 dq.pop_front();
             }
             
-            while(!dq.empty() && pref_sum[dq.back()] >= pref_sum[i]) {
+            while(!dq.empty() && pref_sum[dq.back()] >= pref_sum[i]){
                 dq.pop_back();
             }
             
