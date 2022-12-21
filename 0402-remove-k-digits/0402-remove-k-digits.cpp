ebe14985b91f3e1783627346790195177dfc3fb4
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-20 23:52:32 -0500

    Time: 21 ms (64.03%), Space: 8.6 MB (81.33%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,56 +1,42 @@
 class Solution {
 public:
-    string removeKdigits(string num, int k) {
-        // number of operation greater than length we return an empty string
-        if(num.length() <= k)   
+    string removeKdigits(string& num, int k) {
+        if(k >= num.size()){
             return "0";
+        }
         
-        // k is 0 , no need of removing /  preforming any operation
-        if(k == 0)
+        if(k == 0){
             return num;
+        }
         
-        string res = "";// result string
-        stack <char> s; // char stack
+        stack<char> stk;    // Maintain a monotonic stack
+        stk.push(num[0]);
         
-        s.push(num[0]); // pushing first character into stack
-        
-        for(int i = 1; i<num.length(); ++i)
-        {
-            while(k > 0 && !s.empty() && num[i] < s.top())
-            {
-                // if k greater than 0 and our stack is not empty and the upcoming digit,
-                // is less than the current top than we will pop the stack top
-                --k;
-                s.pop();
+        for(int i=1; i<num.size(); i++){
+            while(!stk.empty() && stk.top() > num[i] && k>0){
+                stk.pop();
+                k--;
             }
+            stk.push(num[i]);
             
-            s.push(num[i]);
-            
-            // popping preceding zeroes
-            if(s.size() == 1 && num[i] == '0')
-                s.pop();
+            if(stk.size() == 1 && num[i] == '0'){
+                stk.pop();   
+            }
         }
         
-        while(k && !s.empty())
-        {
-            // for cases like "456" where every num[i] > num.top()
-            --k;
-            s.pop();
+        // If the entire stack is monotonic, pop the last k elements
+        while(!stk.empty() && k > 0){
+            stk.pop();
+            k--;
         }
         
-        while(!s.empty())
-        {
-            res.push_back(s.top()); // pushing stack top to string
-            s.pop(); // pop the top element
+        string res = "";
+        while(!stk.empty()){
+            res.push_back(stk.top());
+            stk.pop();
         }
+        reverse(res.begin(), res.end());
         
-        reverse(res.begin(),res.end()); // reverse the string 
-        
-        if(res.length() == 0)
-            return "0";
-        
-        return res;
-        
-        
+        return res == "" ? "0" : res;
     }
 };
