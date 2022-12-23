5a0acf777dd8fc83133fb60e65177af80789ea27
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-23 12:22:36 -0500

    Time: 63 ms (21.66%), Space: 16.8 MB (86.82%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int n = nums.size(), mask=0, res=0;
         
-        for (int i=0; i<32; i++){
-            mask = 1<<i;
-            int count = 0;
-            for (int j=0; j<n; j++){
-                if ((nums[j]&mask)!=0){
-                    count++;
-                }
-            }
-            if (count%2!=0){
-                res = res|mask;
-            }
+        int xor_num = 0;
+        
+        for(int i=0; i<nums.size(); i++){
+            xor_num ^= nums[i];
         }
-        return res;     
+        return xor_num;
     }
 };
