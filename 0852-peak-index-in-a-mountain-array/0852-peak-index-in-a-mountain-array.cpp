cb19eff8597517fab3266b73cd6ec1cb43915c87
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-24 18:41:55 -0500

    Time: 141 ms (72.05%), Space: 59.7 MB (20.02%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -5,15 +5,16 @@ public:
         int lo = 0, hi = arr.size()-1, mid;
         
         while(lo<hi){
+            mid = lo + (hi-lo+1)/2;
             
-            mid = lo + (hi-lo)/2;
-            if(arr[mid] > arr[mid+1])
-                hi = mid;
-    
-            else
-                lo = mid+1;
+            if(arr[mid-1] < arr[mid]){
+                lo = mid;
+            }
+            else{
+                hi = mid-1;
+            }
         }
-
-        return lo;        
+        
+        return lo;
     }
 };
