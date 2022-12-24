87421bcd0d8116bb39bfa2624ca10b66e6bad035
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-24 11:53:51 -0500

    Time: 49 ms (57.57%), Space: 20.1 MB (5.10%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,56 +10,59 @@
  */
 class Solution {
 public:
-    ListNode* merge(ListNode* list1, ListNode* list2){
-        ListNode* out = new ListNode(INT_MIN);
-        ListNode* temp = out;
+    ListNode* merge2Lists(ListNode*& list1, ListNode*& list2){
         
-        while(list1 != NULL && list2 != NULL){
-    
-            if(list1->val < list2->val){
-                temp->next = new ListNode(list1->val);
-                temp = temp->next;
-                list1 = list1->next;
+        ListNode* res = new ListNode(INT_MIN);
+        ListNode* temp = res;
+        
+        ListNode* temp1 = list1;
+        ListNode* temp2 = list2;
+        
+        while(temp1 != NULL && temp2 != NULL){
+            
+            if(temp1->val < temp2->val){
+                temp->next = new ListNode(temp1->val);
+                temp1 = temp1->next;
             }
             else{
-                temp->next = new ListNode(list2->val);
-                temp = temp->next;
-                list2 = list2->next;
+                temp->next = new ListNode (temp2->val);
+                temp2 = temp2->next;
             }
+            temp = temp->next;
         }
         
-        if(list1 != NULL){
-            temp->next = list1;
+        while(temp1 != NULL){
+            temp->next = new ListNode(temp1->val);
+            temp1 = temp1->next;
+            temp = temp->next;
         }
         
-        if(list2 != NULL){
-            temp->next = list2;
+        while(temp2 != NULL){
+            temp->next = new ListNode(temp2->val);
+            temp2 = temp2->next;
+            temp = temp->next;
         }
-        
-        return out->next;
+        return res->next;
     }
+    
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        
         if(lists.size() == 0){
             return NULL;
         }
-        
+
         while(lists.size() > 1){
-            
-            vector<ListNode*> temp;
-            
+            vector<ListNode*> res;
             int i, n = lists.size();
-            for(i=1; i<n; i+=2){
-                
-                temp.push_back(merge(lists[i-1], lists[i]));
+            
+            for(i=1; i<n; i+= 2){
+                res.push_back(merge2Lists(lists[i-1], lists[i]));
             }
             
             if(i == n){
-                temp.push_back(lists[n-1]);
+                res.push_back(lists[n-1]);
             }
-            lists = temp;
+            lists = res;
         }
-        
         return lists[0];
     }
 };
