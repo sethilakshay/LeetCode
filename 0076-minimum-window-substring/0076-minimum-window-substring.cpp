0649cb6f1738b7a20841f211c4a403e5c2f540a7
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-24 02:08:25 -0500

    Time: 35 ms (61.34%), Space: 7.8 MB (79.92%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        // Edge case
+        if(t == "" || s.size() == 0){
+            return "";
+        }
+        
+        unordered_map<char, int> tMap;
+        unordered_map<char, int> sMap;
+        
+        for(char c: t){
+            tMap[c]++;
+        }
+
+        int need = tMap.size(), have = 0;
+        int i = 0, j = 0, resLen = INT_MAX, start = -1;
+        
+        while(j < s.size()){
+
+            if(tMap.find(s[j]) != tMap.end()){
+                sMap[s[j]]++;
+                
+                if(tMap[s[j]] == sMap[s[j]]){
+                    have++;
+                }
+            }
+            
+            while(have == need && i <= j){
+                
+                if(resLen > j-i+1){
+                    resLen = j-i+1;
+                    start = i;
+                }
+                
+                if(tMap.find(s[i]) != tMap.end()){
+                    sMap[s[i]]--;
+                    
+                    if(sMap[s[i]] < tMap[s[i]]){
+                        have--;
+                    }
+                }
+                i++;
+            }
+            j++;
+        }
+
+        return start != -1 ? s.substr(start, resLen) : "";
+        
+    }
+};
