7b072cb848bb4e5428dbcfafa9b5df8abf3a2e6c
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-24 15:53:35 -0500

    Time: 113 ms (75.06%), Space: 14.6 MB (55.53%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size(), i, j;
         
-        vector<int> dp (amount+1, INT_MAX-1);
-        
-        //When Amount=0, we need 0 coins
+        vector<int> dp(amount + 1, INT_MAX-1);
+        // O coins needed to make sum 0
         dp[0] = 0;
         
-        for(i=0; i<n; i++){
+        for(int i=0; i<coins.size(); i++){
             
-            for(j=1; j<=amount; j++){
+            for(int j=1; j<=amount; j++){
                 
-                if(j-coins[i]>=0)
-                    dp[j] = min(dp[j], 1+dp[j-coins[i]]);
+                if(j - coins[i] >= 0)
+                    dp[j] = min(dp[j], dp[j-coins[i]] + 1);
             }
         }
         return dp[amount] == INT_MAX-1 ? -1 : dp[amount];
