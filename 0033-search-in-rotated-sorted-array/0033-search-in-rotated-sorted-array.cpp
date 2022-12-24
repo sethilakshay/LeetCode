7e4b024c009103ba8399789eb17f883cc56b5372
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-24 17:17:02 -0500

    Time: 5 ms (69.53%), Space: 11.2 MB (31.75%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
-    int binSearch(int lo, int hi, vector<int>& nums, int& target){
-        
+    int binSearch(int lo, int hi, int target, vector<int>& nums){
         int mid;
+        
         while(lo<=hi){
-            mid = lo + (hi-lo)/2;
-            if(nums[mid] == target)
+            mid  = lo + (hi-lo)/2;
+            
+            if(nums[mid] == target){
                 return mid;
-            else if(nums[mid] > target)
-                hi = mid-1;
-            else
+            }
+            else if(nums[mid] < target){
                 lo = mid+1;
+            }
+            else{
+                hi = mid-1;
+            }
         }
         return -1;
     }
-    
     int search(vector<int>& nums, int target) {
         
         int lo = 0, hi = nums.size()-1, mid;
+        int pivot;
         
-        //To check if array is rotated and if yes, place lo at the smallest position
         while(lo<hi){
             
             if(nums[lo] > nums[hi]){
-                
                 mid = lo + (hi-lo)/2;
-                
-                if((nums[lo]+nums[hi])/2 >= nums[mid]){
+
+                if(nums[mid] <= (nums[lo] + nums[hi])/2){
                     hi = mid;
                 }
                 else{
@@ -38,9 +40,11 @@ public:
             }
         }
         
-        if(target<=nums[nums.size()-1])
-            return binSearch(lo, nums.size()-1, nums, target);
-        else
-            return binSearch(0, lo-1, nums, target); 
+        if(target >= nums[lo] && target <= nums[nums.size()-1]){
+            return binSearch(lo, nums.size()-1, target, nums);
+        }
+        else{
+            return binSearch(0, lo-1, target, nums);
+        }
     }
 };
