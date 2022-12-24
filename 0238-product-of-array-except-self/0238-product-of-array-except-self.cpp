41d27930f0550e1622a949a7c4405cd6d1f39a54
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-24 16:06:27 -0500

    Time: 36 ms (68.78%), Space: 25.1 MB (22.17%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -5,16 +5,16 @@ public:
         int n = nums.size();
         vector<int> left(n, 1);
         vector<int> right(n, 1);
-        vector<int> res(n);
         
         for(int i=1; i<n; i++){
-            left[i] = nums[i-1] * left[i-1];
+            left[i] = left[i-1]*nums[i-1];
         }
         
         for(int i=n-2; i>=0; i--){
-            right[i] = nums[i+1] * right[i+1];
+            right[i] = right[i+1]*nums[i+1];
         }
         
+        vector<int> res(n);
         for(int i=0; i<n; i++){
             res[i] = left[i]*right[i];
         }
