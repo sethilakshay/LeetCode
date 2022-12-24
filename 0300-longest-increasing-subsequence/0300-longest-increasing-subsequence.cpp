91345617518f46989308ee5323115cf2c4e69f1b
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-24 18:10:25 -0500

    Time: 496 ms (46.65%), Space: 10.7 MB (29.47%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,32 +2,19 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         
-        vector<int> vect;
-        vect.push_back(nums[0]);
+        int n = nums.size(), res;
+        vector<int> dp(n, 1);
         
-        for(int i=1; i<nums.size(); i++){
+        for(int i=0; i<n; i++){
             
-            if(vect.back() < nums[i]){
-                vect.push_back(nums[i]);
-            }
-            else if (vect.back() > nums[i]){
-                //Task is to find the element in arr just bigger or equal to nums[i]
-                int lo = 0, hi = vect.size()-1, mid;
+            for(int j=i; j>=0; j--){
                 
-                while(lo<hi){
-                    mid = lo + (hi-lo)/2;
-                    
-                    if(vect[mid] >= nums[i])
-                        hi = mid;
-                    else
-                        lo = mid+1;
+                if(nums[j] < nums[i]){
+                    dp[i] = max(1+dp[j], dp[i]);
                 }
-                
-                if(vect[hi] >= nums[i])
-                    vect[hi] = nums[i];
-
+                res = max(dp[i], res);
             }
         }
-        return vect.size();
+        return res;
     }
 };
