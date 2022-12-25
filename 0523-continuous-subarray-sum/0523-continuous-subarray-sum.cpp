56dabaaf7aeae1506b8d3e343323b4cf6982298d
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-25 16:24:22 -0500

    Time: 245 ms (90.33%), Space: 113.7 MB (69.00%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        
+        // Hash Map to store the remainders and indices
         unordered_map<int, int> hash_map;
-        
-        // When no element in the array is considered, we have 0 remainder and index = -1
+        // When empty array, rem is zero and possible index = -1
         hash_map[0] = -1;
         
-        int cum_sum = 0, rem = 0;
+        long cum_sum = 0, n = nums.size(), rem = 0;
         
-        for(int i=0; i<nums.size(); i++){
-            
+        for(int i=0; i<n; i++){
             cum_sum += nums[i];
-            rem = cum_sum%k;
             
-            if(rem < 0){
+            rem = cum_sum%k;
+            if(rem > 0){
                 rem += k;
             }
             
