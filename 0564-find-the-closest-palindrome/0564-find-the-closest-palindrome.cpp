3396a7756173720a656ab770198620cbe164477a
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-25 17:47:54 -0500

    Time: 0 ms (100.00%), Space: 6.4 MB (30.29%) - LeetHub

--- 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.cpp
+++ 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.cpp
@@ -1,50 +1,53 @@
 class Solution {
 public:
     string nearestPalindromic(string n) {
+        // to store all the possible palindromes
+        vector<long> posPal;
+        
         int digits = n.size();
         
-        if(digits==1)
-            return to_string(stoi(n)-1);
+        if(digits == 1){
+            return to_string(stol(n)-1);
+        }
         
-        vector<long> res;
-        res.push_back(pow(10, digits)+1);  //Case 1: 1, 11, 101, 1001, 10001.....
-        res.push_back(pow(10, digits-1)+1); //Case 1: 1, 11, 101, 1001, 10001.....
+        // Case 1: 1, 11, 101, 1001, 10001, 100001......
+        posPal.push_back(pow(10, digits) + 1);
+        posPal.push_back(pow(10, digits-1) + 1);
         
-        res.push_back(pow(10, digits)-1);  //Case 2: 9, 99, 999, 9999.......
-        res.push_back(pow(10, digits-1)-1);  //Case 2: 9, 99, 999, 9999.......
+        // Case 2: 9, 99, 999, 9999, 99999, 999999......
+        posPal.push_back(pow(10, digits) - 1);
+        posPal.push_back(pow(10, digits-1) - 1);
         
-        // Case 3: Dividing the prefix at mid-point and making that a palindrome
-        // 3 Cases: to find mid-1, mid and mid+1 palindromes
-        int mid = (digits+1)/2;
-        long prefix = stol(n.substr(0, mid));
+        // Case 3: Dividing the number at middle and making its 3 palidnromes
+        string mid = n.substr(0, (digits+1)/2);
+        
         
         for(int i=0; i<3; i++){
-            string pref = to_string(prefix-1+i);
-            string pref_rev = pref; // Normal case for even digits
+            long mid_num = stol(mid);
+            mid_num += i - 1;
             
-            // In case if we have an odd number, then other half will have 1 less digit
-            if(digits%2 != 0)
+            string prefix = to_string(mid_num);
+            string pref_rev = prefix;
+            
+            if(digits%2 != 0){
                 pref_rev = pref_rev.substr(0, pref_rev.size()-1);
-
+            }
             reverse(pref_rev.begin(), pref_rev.end());
-            string temp_res = pref + pref_rev;
-            
-            res.push_back(stol(temp_res));
+            posPal.push_back(stol(prefix + pref_rev));
         }
         
-        long diff = LONG_MAX, num = stol(n), ans = 0;
+        long diff = LONG_MAX, num = stol(n), res;
         
-        for(int i=0; i<7; i++){ 
+        for(int i=0; i<posPal.size(); i++){
             
-            if (abs(res[i]-num) < diff && res[i]!=num){
-                diff = abs(res[i]-num);
-                ans = res[i];
+            if(abs(posPal[i] - num) < diff && posPal[i] != num){
+                diff = abs(posPal[i] - num);
+                res = posPal[i];
             }
-            
-            else if((abs(res[i]-num) == diff) && (res[i]<ans)){
-                ans = res[i];
+            else if(abs(posPal[i] - num) == diff && posPal[i] < res){
+                res = posPal[i];
             }
         }
-        return to_string(ans);
+        return to_string(res);
     }
 };
