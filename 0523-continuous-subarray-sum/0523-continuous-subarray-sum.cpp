abc2c17c4dbf1eea54262fd35bb11ac873f6e19e
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-24 22:43:04 -0500

    Time: 268 ms (84.59%), Space: 113.9 MB (45.43%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -2,23 +2,27 @@ class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
         
-        //Key to store the remainder and value to store the index
         unordered_map<int, int> hash_map;
-        int res = 0, run_sum = 0, rem;
         
-        //Intializing remainder 0 to -1 as an empty array will also have 0 remainder
+        // When no element in the array is considered, we have 0 remainder and index = -1
         hash_map[0] = -1;
-        for(int i = 0; i<nums.size(); i++){
-            run_sum += nums[i];
+        
+        int cum_sum = 0, rem = 0;
+        
+        for(int i=0; i<nums.size(); i++){
             
-            rem = run_sum%k;
-            if(rem<0)
-                rem += rem+k;
+            cum_sum += nums[i];
+            rem = cum_sum%k;
+            
+            if(rem < 0){
+                rem += k;
+            }
             
             if(hash_map.find(rem) != hash_map.end()){
                 if(i - hash_map[rem] >= 2)
                     return true;
-            }else{
+            }
+            else{
                 hash_map[rem] = i;
             }
         }
