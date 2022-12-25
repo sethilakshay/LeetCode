6a8c6665b4b9bd1cbf68ba7f04da743605b67018
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-25 18:05:12 -0500

    Time: 6 ms (64.55%), Space: 11.2 MB (32.18%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -2,31 +2,32 @@ class Solution {
 public:
     int binSearch(int lo, int hi, int target, vector<int>& nums){
         int mid;
-        while(lo <= hi){
+        
+        while(lo<=hi){
             mid = lo + (hi-lo)/2;
-            
             if(nums[mid] == target){
                 return mid;
             }
-            else if(nums[mid] < target){
-                lo = mid+1;
+            else if(nums[mid] > target){
+                hi = mid-1;
             }
             else{
-                hi = mid-1;
+                lo = mid+1;
             }
         }
         return -1;
     }
+    
     int search(vector<int>& nums, int target) {
         
-        int lo = 0, hi = nums.size()-1, mid;
+        int n = nums.size(), lo = 0, hi = n-1, mid;
         while(lo<hi){
             if(nums[lo] < nums[hi]){
                 break;
             }
-
-            mid = lo + (hi-lo)/2;            
-            if(nums[mid] >= (nums[lo] + nums[hi])/2){
+            
+            mid = lo+(hi-lo)/2;
+            if(nums[mid] >= (nums[hi] + nums[lo])/2){
                 lo = mid+1;
             }
             else{
@@ -34,8 +35,8 @@ public:
             }
         }
         
-        if(target >= nums[lo] && target <= nums[nums.size()-1]){
-            return binSearch(lo, nums.size()-1, target, nums);
+        if(target >= nums[lo] && target <= nums[n-1]){
+            return binSearch(lo, n-1, target, nums);
         }
         else{
             return binSearch(0, lo-1, target, nums);
