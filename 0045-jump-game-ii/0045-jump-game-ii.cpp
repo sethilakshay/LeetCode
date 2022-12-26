f4b6fd6dd35b9510f7b66db65178e46a9f5620f1
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-25 22:09:53 -0500

    Time: 1950 ms (5.01%), Space: 17.3 MB (30.64%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
+    // TC: O(N^2)
+    // Using Dynamic Programming
     int jump(vector<int>& nums) {
         
         int n = nums.size();
         vector<int> dp(n, INT_MAX-1);
+        
+        // Since no jump is needed to reach index 0
         dp[0] = 0;
         
         for(int i=1; i<n; i++){
-            
             for(int j=i-1; j>=0; j--){
                 if(j + nums[j] >= i){
                     dp[i] = min(dp[i], 1+dp[j]);
