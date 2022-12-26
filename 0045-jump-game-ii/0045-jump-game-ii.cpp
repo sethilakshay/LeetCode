63dbcb4ba954cd5d3ff5597586419e24a50bb309
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-25 22:20:23 -0500

    Time: 15 ms (93.97%), Space: 16.6 MB (58.36%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    // TC: O(N^2)
-    // Using Dynamic Programming
+    // TC: O(n)
+    // Greedy Solution in linear time
     int jump(vector<int>& nums) {
         
-        int n = nums.size();
-        vector<int> dp(n, INT_MAX-1);
+        int res = 0, n = nums.size();
+        int left = 0, right = 0, farthest = 0;
         
-        // Since no jump is needed to reach index 0
-        dp[0] = 0;
-        
-        for(int i=1; i<n; i++){
-            for(int j=i-1; j>=0; j--){
-                if(j + nums[j] >= i){
-                    dp[i] = min(dp[i], 1+dp[j]);
-                }
+        while(right < n-1){
+            farthest = 0;
+            
+            for(int i=left; i<=right; i++){
+                farthest = max(farthest, i-right+nums[i]);
             }
+            
+            left = right+1;
+            right += farthest;
+            res++;
         }
-        return dp[n-1];
+        return res;
     }
 };
