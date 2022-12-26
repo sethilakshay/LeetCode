8096cffd6ea88ce4c2b6bb9667f6eaf12310375b
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-25 20:34:01 -0500

    Time: 9 ms (87.81%), Space: 7.4 MB (56.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,23 @@
 class Solution {
 public:
+    // TC: O(N)
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return (s==t);
-    }
+        if (s.size() != t.size()) {
+            return false;
+        }
+        
+        vector<int> dict(26, 0);
+        for (int i = 0; i < s.size(); i++) {
+            dict[s[i] - 'a']++;
+        }
+        
+        for (int j = 0; j < t.size(); j++) {
+            dict[t[j] - 'a']--;
+            if (dict[t[j] - 'a'] < 0) {
+                return false;
+            }
+        }
+            
+        return true;
+    } 
 };
