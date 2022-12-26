65ecf07f19ff9ab3443f478b74e1a057703531ca
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-25 20:00:40 -0500

    Time: 159 ms (58.72%), Space: 70.4 MB (55.68%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,68 +1,71 @@
 class Solution {
 public:
+    
     struct TrieNode{
         TrieNode* child[26] = {NULL};
         string word = "";
     };
-    
+    // Initializing the TrieNode
     TrieNode* root = new TrieNode();
     
-    //Function to build the tree
-    void buildTree(vector<string>& products){
-        
-        for(int i=0; i<products.size(); i++){
-            
-            TrieNode* temp = root;
-            
-            for(char c: products[i]){
-                if(temp->child[c-'a'] == NULL){
-                    temp->child[c-'a'] = new TrieNode();
-                }
-                temp = temp->child[c-'a'];
+    void buildTrie(string& str){
+        TrieNode* temp = root;
+        for(char c: str){
+            if(temp->child[c-'a'] == NULL){
+                temp->child[c-'a'] = new TrieNode();
             }
-            temp->word = products[i];
+            temp = temp->child[c-'a'];
         }
+        temp->word = str;
     }
     
-    void DFS(TrieNode* root, int limit, vector<string>& res){
-        if(root == NULL)
+    void searchTrie(int& idx, vector<vector<string>>& res, TrieNode* node){
+        if(node == NULL){
             return;
+        }
         
-        if(res.size() == limit)
+        if(res[idx].size() == 3){
             return;
+        }
         
-        if(root->word.size() > 0)
-            res.push_back(root->word);
+        if(node->word != ""){
+            res[idx].push_back(node->word);
+        }
         
         for(int i=0; i<26; i++){
-            DFS(root->child[i], limit, res);
+            TrieNode* temp = node->child[i];
+            searchTrie(idx, res, temp);
         }
     }
     
-    vector<string> findWords(TrieNode* root, int limit){
-        vector<string> res;
-        DFS(root, limit, res);
-        return res;
-    }
-    
-    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-        //Building the Trie Tree
-        buildTree(products);
+    void dfs(string& searchWord, vector<vector<string>>& res){
         TrieNode* temp = root;
+        int idx = -1;
         
-        vector<vector<string>> res;
-
         for(char c: searchWord){
-
+            res.push_back(vector<string>());
+            idx++;
+            
             if(temp != NULL && temp->child[c-'a'] != NULL){
                 temp = temp->child[c-'a'];
-                res.push_back(findWords(temp, 3));
             }
             else{
                 temp = NULL;
-                res.push_back({});
+                continue;
             }
+
+            searchTrie(idx, res, temp);
         }
+    }
+    
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        for(string str: products){
+            buildTrie(str);
+        }
+        
+        vector<vector<string>> res;    
+        dfs(searchWord, res);
+    
         return res;
     }
 };
