0a14a6fbdda936e7f21e3235ba03f06a78e95509
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2022-12-26 13:30:29 -0500

    Time: 10 ms (81.56%), Space: 11.3 MB (29.39%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,28 +17,33 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        unordered_map<Node*, Node*> list_map;
+        // Why do we create a hash map to store nodes and not simply assign by doing equal to
+        // Bcz assigning will point to the location of the stored value and node, not actual value
+        // We want to create a copy completely seperate from the original, not pointing to the orig
+        // Think of diff between pandas.copy(deep=True) vs pandas.copy() [shallow copy]
+        
+        unordered_map<Node*, Node*> hash_map;
         Node* temp = head;
         
         while(temp != NULL){
-            list_map[temp] = new Node(temp->val);
+            hash_map[temp] = new Node(temp->val);
             temp = temp->next;
         }
         
-        list_map[NULL] = NULL;
+        hash_map[NULL] = NULL;
         
-        temp = head;
         Node* res = new Node(INT_MIN);
         Node* temp1 = res;
         
+        temp = head;
+        
         while(temp != NULL){
+            temp1->next = hash_map[temp];
+            temp1->next->next = hash_map[temp->next];
+            temp1->next->random = hash_map[temp->random];
             
-            temp1->next = list_map[temp];
-            temp1->next->next = list_map[temp->next];
-            temp1->next->random = list_map[temp->random];
-            
-            temp = temp->next;
             temp1 = temp1->next;
+            temp = temp->next;
         }
         return res->next;
     }
