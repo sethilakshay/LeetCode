b3e55f98490fb5a833d32ae057457c63f4a3b399
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2023-01-02 18:38:48 -0500

    Time: 42 ms (31.99%), Space: 13.7 MB (9.68%) - LeetHub

--- 1366-rank-teams-by-votes/1366-rank-teams-by-votes.cpp
+++ 1366-rank-teams-by-votes/1366-rank-teams-by-votes.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     string rankTeams(vector<string>& votes) {
-        int n = votes[0].size();
-        vector<vector<int>> freq(n, vector<int>(26, 0));
-        string out = votes[0];
         
-        for(int i=0; i<n; i++){
-            for(string vote: votes){
-                freq[i][vote[i] - 'A']++;
+        vector<vector<int>> freq(votes[0].size(), vector<int>(26, 0));
+        
+        for(int i=0; i<votes.size(); i++){
+            for(int j=0; j<votes[0].size(); j++){
+                freq[j][votes[i][j] - 'A']++;
             }
         }
         
-        sort(out.begin(), out.end(), [freq](char& a, char& b){
+        string res = votes[0];
+        
+        sort(res.begin(), res.end(), [freq](char& a, char& b){
+            
             for(int i=0; i<freq.size(); i++){
                 if(freq[i][a - 'A'] == freq[i][b - 'A']){
-                    continue;
+                    continue;;
                 }
-                else{
-                    return freq[i][a - 'A'] > freq[i][b - 'A'];
-                }
+                return freq[i][a - 'A'] > freq[i][b - 'A'];
             }
-            return a<b;
+            return a < b;
         });
-        
-        return out;
+        return res;
     }
 };
