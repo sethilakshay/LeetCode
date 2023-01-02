9f052ccd1628857028fa65e9ca47455d0d8dd230
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2023-01-02 18:06:35 -0500

    Time: 60 ms (37.18%), Space: 19.3 MB (61.86%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         
-        int zero_ptr = 0;
+        int j = 0;
         for(int i=0; i<nums.size(); i++){
             if(nums[i] != 0){
-                swap(nums[zero_ptr], nums[i]);
-                zero_ptr++;
+                swap(nums[i], nums[j]);
+                j++;
             }
-        }
+        }       
     }
 };
