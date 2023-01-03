a82ed3c3b37a50a74fd923a3b728380f2483287e
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2023-01-03 18:50:00 -0500

    Time: 239 ms (52.11%), Space: 36.5 MB (48.83%) - LeetHub

--- 0730-count-different-palindromic-subsequences/0730-count-different-palindromic-subsequences.cpp
+++ 0730-count-different-palindromic-subsequences/0730-count-different-palindromic-subsequences.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     int countPalindromicSubsequences(string s) {
         
-        int n = s.size();
-        vector<vector<long>> dp(n, vector<long>(n, 0));
-        long mod = 1e9+7;
+        int n = s.size(), mod = 1e9+7;
+        
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
         
         for(int i=n-1; i>=0; i--){
             dp[i][i] = 1;
@@ -12,20 +13,22 @@ public:
             for(int j=i+1; j<n; j++){
                 
                 if(s[i] != s[j]){
-                    dp[i][j] = dp[i+1][j] + dp[i][j-1] - dp[i+1][j-1];
+                    dp[i][j] = dp[i][j-1] + dp[i+1][j] - dp[i+1][j-1];
                 }
+                // if s[i] == s[j] then 3 cases
                 else{
                     dp[i][j] = 2*dp[i+1][j-1];
                     int left = i+1, right = j-1;
                     
-                    while(left <= j-1 && s[left] != s[j]){
+                    while(left <= j-1 && s[left] != s[i]){
                         left++;
                     }
                     
-                    while(right >= i+1 && s[right] != s[i]){
+                    while(i+1 <= right && s[right] != s[j]){
                         right--;
                     }
                     
+                    // Case 1: means no char = s[i] in Palindrome
                     if(left > right){
                         dp[i][j] += 2;
                     }
@@ -34,9 +37,8 @@ public:
                     }
                     else{
                         dp[i][j] -= dp[left+1][right-1];
-                    }   
+                    }
                 }
-                
                 dp[i][j] %= mod;
                 if(dp[i][j] < 0){
                     dp[i][j] += mod;
