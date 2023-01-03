0a686899194bd519e3300034199f9c32da2f9cf5
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2023-01-03 16:49:59 -0500

    Time: 6 ms (11.96%), Space: 6.5 MB (9.78%) - LeetHub

--- 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.cpp
+++ 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
     string nearestPalindromic(string n) {
+        // to store all the possible palindromes
+        vector<long> posPal;
         
-        vector<long> res;
         int digits = n.size();
         
         if(digits == 1){
             return to_string(stol(n)-1);
         }
         
-        //Case 1: 1, 11, 101, 1001, 10001...
-        res.push_back(pow(10, digits) + 1);
-        res.push_back(pow(10, digits-1) + 1);
+        // Case 1: 1, 11, 101, 1001, 10001, 100001......
+        posPal.push_back(pow(10, digits) + 1);
+        posPal.push_back(pow(10, digits-1) + 1);
         
-        //Case 2: 9, 99, 999, 9999, 99999...
-        res.push_back(pow(10, digits) - 1);
-        res.push_back(pow(10, digits-1) - 1);
+        // Case 2: 9, 99, 999, 9999, 99999, 999999......
+        posPal.push_back(pow(10, digits) - 1);
+        posPal.push_back(pow(10, digits-1) - 1);
         
-        //Case 3
+        // Case 3: Dividing the number at middle and making its 3 palidnromes
         string mid = n.substr(0, (digits+1)/2);
         
-        for(long i=0; i<3; i++){
+        
+        for(int i=0; i<3; i++){
             long mid_num = stol(mid);
             mid_num += i - 1;
             
@@ -31,21 +33,21 @@ public:
                 pref_rev = pref_rev.substr(0, pref_rev.size()-1);
             }
             reverse(pref_rev.begin(), pref_rev.end());
-            res.push_back(stol(prefix + pref_rev));
+            posPal.push_back(stol(prefix + pref_rev));
         }
         
-        long diff = LONG_MAX, ans, num = stol(n);
+        long diff = LONG_MAX, num = stol(n), res;
         
-        for(int i=0; i<res.size(); i++){
+        for(int i=0; i<posPal.size(); i++){
             
-            if(abs(res[i] - num) < diff && res[i] != num){
-                diff = abs(res[i] - num);
-                ans = res[i];
+            if(abs(posPal[i] - num) < diff && posPal[i] != num){
+                diff = abs(posPal[i] - num);
+                res = posPal[i];
             }
-            else if(abs(res[i] - num) == diff && res[i] < ans){
-                ans = res[i];
+            else if(abs(posPal[i] - num) == diff && posPal[i] < res){
+                res = posPal[i];
             }
         }
-        return to_string(ans);
+        return to_string(res);
     }
 };
