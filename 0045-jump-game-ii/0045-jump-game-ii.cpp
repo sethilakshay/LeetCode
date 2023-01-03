211d4e0314550badca89e1eddf8643172a8c47c4
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2023-01-03 17:47:18 -0500

    Time: 12 ms (95.74%), Space: 16.6 MB (83.87%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -9,10 +9,10 @@ public:
             
             int max_jump = 0;
             for(int i=left; i<=right; i++){
-                max_jump = max(max_jump, i + nums[i] - right);
+                max_jump = max(max_jump, nums[i] + i - right);
             }
             
-            left = right + 1;
+            left = right+1;
             right += max_jump;
             res++;
         }
