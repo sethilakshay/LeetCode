be02d167cce108f850090c631036fc7a5b559d1d
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2023-01-07 05:10:17 -0500

    Time: 14 ms (85.96%), Space: 8.9 MB (81.87%) - LeetHub

--- 2268-minimum-number-of-keypresses/2268-minimum-number-of-keypresses.cpp
+++ 2268-minimum-number-of-keypresses/2268-minimum-number-of-keypresses.cpp
@@ -2,31 +2,28 @@ class Solution {
 public:
     int minimumKeypresses(string s) {
         
-        vector<int> freq(26, 0);
+        int n = 26, res = 0, cnt = 0;
+        vector<int> freq(n, 0);
         
+        //Frequency array to keep track of how much each letter has been pressed
         for(char c: s){
-            freq[c - 'a']++;
+            freq[c-'a']++;
         }
         
-        // Sorting in Descending order
+        //Sorting in descending order to give priority to characters having high frequency
         sort(freq.begin(), freq.end(), [](int& a, int& b){
             return b<a;
         });
         
-        int res = 0, cnt = 0;
-        
+        //Looping through the frequency array
         for(int i=0; i<26; i++){
-            
-            if(freq[i] == 0){
-                break;
-            }
-            
-            if(i%9 == 0){
+            //Increasing count after every 9 iterations
+            if(i%9 == 0)
                 cnt++;
-            }
             
             res += cnt*freq[i];
         }
+        
         return res;
     }
 };
