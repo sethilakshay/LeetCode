473299b4a2d4e8f147ecc6fdbedc1d3035012bd2
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2023-01-06 22:12:28 -0500

    Time: 69 ms (96.47%), Space: 35.3 MB (96.27%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,32 +1,43 @@
 class Solution {
 public:
     int subarraysWithAtmostKDistinct(vector<int>& nums, int k){
-        int i, j, n = nums.size(), cnt_dist = 0, res=0;
+        
+        int i = 0, j = 0, res = 0, n = nums.size(), cnt_dist = 0;
         vector<int> freq(n, 0);
-
-        j=0;
-        for(i=0; i<n; i++){
-            while(j<n && cnt_dist<=k){
+        
+        while(i < n){
+            
+            while(j<n && cnt_dist <= k){
                 
-                if (freq[nums[j]-1]==0) 
+                if(freq[nums[j] - 1] == 0){
                     cnt_dist++;
+                }
                 
-                freq[nums[j]-1]++;
+                freq[nums[j] - 1]++;
                 j++;
             }
-            if (cnt_dist>k)
-                    res += j-i-1;
-            else
+            
+            if(cnt_dist > k){
+                res += j-i-1;
+            }
+            else{
                 res += j-i;
+            }
             
             freq[nums[i]-1]--;
-            if (freq[nums[i]-1]==0)
-                     cnt_dist--;  
+            if(freq[nums[i]-1] == 0){
+                cnt_dist--;
+            }
+            i++;
         }
         return res;
     }
+    
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        return subarraysWithAtmostKDistinct(nums, k) - subarraysWithAtmostKDistinct(nums, k-1);
+        // Approach: Rather than finding the exact k
+        // Why not find the distinct subarrays with atmost K distinct
+        // To get our answer, simply subtract the atmost k-1 distinct
         
+        return subarraysWithAtmostKDistinct(nums, k) - subarraysWithAtmostKDistinct(nums, k-1);
     }
 };
