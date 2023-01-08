370b81fef777f61801777c84586dc3c3e2dba924
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2023-01-08 14:22:38 -0500

    Time: 255 ms (82.83%), Space: 60.3 MB (86.13%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -3,15 +3,14 @@ public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         
         sort(points.begin(), points.end(), [](vector<int>& a, vector<int>& b){
-            return a[0]*a[0] + a[1]*a[1] < b[0]*b[0] + b[1]*b[1];
+            return (a[0]*a[0] + a[1]*a[1]) < (b[0]*b[0] + b[1]*b[1]);
         });
-            
-        vector<vector<int>> res(k);
+        
+        vector<vector<int>> res;
         
         for(int i=0; i<k; i++){
-            res[i] = points[i];
+            res.push_back(points[i]);
         }
-        
         return res;
     }
 };
