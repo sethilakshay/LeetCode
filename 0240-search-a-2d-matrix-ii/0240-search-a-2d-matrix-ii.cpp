842c7780af51002f8932b51bb45121bc7ef8af47
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2023-01-08 04:40:06 -0500

    Time: 116 ms (80.86%), Space: 14.8 MB (90.57%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        
-        int row = 0, col = matrix[0].size()-1;
-        
-        while(row < matrix.size() && col >= 0){
-            if(matrix[row][col] == target){
+        int row=0, column=matrix[0].size()-1,mid;
+        //Getting the Largest element in the row and smallest in column
+        while(row<=(matrix.size()-1) && column>=0){
+            //checking if this is equal to target
+            if(matrix[row][column]==target){
                 return true;
             }
-            else if(matrix[row][col] < target){
-                row += 1;
+            //if the element(largest in its row and smallest in the column)<target 
+            //then entire row can be discarded!!!
+            else if(matrix[row][column]<target){
+                row+=1;
             }
+            //if the element(largest in its row and smallest in the column)>target 
+            //then entire column can be discarded!!!
             else{
-                col -= 1;
+                column-=1;
             }
         }
-        return false;
+     return false;   
     }
 };
