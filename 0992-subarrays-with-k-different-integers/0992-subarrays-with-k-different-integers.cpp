28305d3d12caa5d093fa500631f3a686e1697569
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2023-01-07 20:44:23 -0500

    Time: 59 ms (98.40%), Space: 35.5 MB (95.59%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -3,12 +3,12 @@ public:
     int subarraysWithAtmostKDistinct(vector<int>& nums, int k){
         
         int i = 0, j = 0, n = nums.size();
-        int cnt_dist = 0, res = 0;
-        
+        int res = 0, cnt_dist = 0;
         vector<int> freq(n, 0);
         
         while(i < n){
-            while(j < n && cnt_dist <= k){
+            while(j<n && cnt_dist <= k){
+                
                 if(freq[nums[j] - 1] == 0){
                     cnt_dist++;
                 }
@@ -24,8 +24,8 @@ public:
                 res += j-i;
             }
             
-            freq[nums[i] - 1]--;
-            if(freq[nums[i] - 1] == 0){
+            freq[nums[i]-1]--;
+            if(freq[nums[i]-1] == 0){
                 cnt_dist--;
             }
             i++;
@@ -34,8 +34,11 @@ public:
     }
     
     int subarraysWithKDistinct(vector<int>& nums, int k) {
+        // Classic 2 Pointers question
+        // Approach: Rather than finding the exact k
+        // Why not find the distinct subarrays with atmost K distinct
+        // To get our answer, simply subtract the atmost k-1 distinct
         
         return subarraysWithAtmostKDistinct(nums, k) - subarraysWithAtmostKDistinct(nums, k-1);
-        
     }
 };
