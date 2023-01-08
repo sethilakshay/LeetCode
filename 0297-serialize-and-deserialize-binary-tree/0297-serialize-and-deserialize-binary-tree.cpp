4bc975f8158a659cf73b20ffaab9b8f3fa635e5c
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2023-01-07 23:35:24 -0500

    Time: 36 ms (99.36%), Space: 28.7 MB (98.06%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
@@ -9,29 +9,49 @@
  */
 class Codec {
 public:
-    // Pre-Order serialization and Deserialization
-    void encode(TreeNode* root, string& out){
-        if(root == NULL){
-            out.push_back('n');
-            out.push_back(',');
-            return;
-        }
-        
-        out += to_string(root->val);
-        out.push_back(',');
-        
-        encode(root->left, out);
-        encode(root->right, out);
-        
-    }
+    // Level-Order Binary Tree serialization and De-serialization
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
         
         string out = "";
-        encode(root, out);
+        if(root == NULL){
+            return out;
+        }
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        out += to_string(root->val);
+        out.push_back(',');
+        
+        while(!q.empty()){
+            
+            int n = q.size();
+            for(int i=0; i<n; i++){
+                TreeNode* temp = q.front();
+                q.pop();
+                
+                if(temp->left != NULL){
+                    q.push(temp->left);
+                    out += to_string(temp->left->val);
+                }
+                else{
+                    out.push_back('n');
+                }
+                out.push_back(',');
+                
+                if(temp->right != NULL){
+                    q.push(temp->right);
+                    out += to_string(temp->right->val);
+                }
+                else{
+                    out.push_back('n');
+                }
+                out.push_back(',');
+            }
+        }
         return out;
     }
-    
+
     TreeNode* buildTree(int& idx, string& data){
         if(idx == data.size()){
             return NULL;
@@ -46,25 +66,43 @@ public:
             return NULL;
         }
         
-        
         string currNum = "";
-        while(data[idx] != 'n' && data[idx] != ','){
-            currNum += data[idx];
+        while(data[idx] != ','){
+            currNum.push_back(data[idx]);
             idx++;
         }
         
-        TreeNode* root = new TreeNode(stoi(currNum));
-        
-        root->left = buildTree(idx, data);
-        root->right = buildTree(idx, data);
-        
-        return root;
+        return new TreeNode(stoi(currNum));
     }
-
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
+        if(data == ""){
+            return NULL;
+        }
+        
         int idx = 0;
-        return buildTree(idx, data);
+        TreeNode* root = buildTree(idx, data);
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            int n = q.size();
+            
+            for(int i=0; i<n; i++){
+                TreeNode* temp = q.front();
+                q.pop();
+                
+                temp->left = buildTree(idx, data);
+                if(temp->left != NULL)
+                    q.push(temp->left);
+                
+                temp->right = buildTree(idx, data);
+                if(temp->right != NULL)
+                    q.push(temp->right);
+            }
+        }
+        return root;
     }
 };
 
