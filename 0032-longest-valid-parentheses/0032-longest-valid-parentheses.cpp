aac053488cf1d28fd9f40826b78c793306021fad
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2023-01-08 19:48:06 -0500

    Time: 0 ms (100.00%), Space: 7.3 MB (46.16%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     int longestValidParentheses(string s) {
         
-        stack<int> stk;
+        int res = 0;
+
+        stack<int> stk;     //To store the indices of '('
         stk.push(-1);
         
-        int res = 0;
-        
         for(int i=0; i<s.size(); i++){
-            
             if(s[i] == '('){
                 stk.push(i);
             }
@@ -18,10 +17,8 @@ public:
                 if(stk.empty()){
                     stk.push(i);
                 }
-                
                 res = max(res, i - stk.top());
             }
-            
         }
         return res;
     }
