9d6ff817f93327034e5e090b02e52427c0969b70
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2023-01-09 02:44:36 -0500

    Time: 45 ms (87.44%), Space: 16 MB (54.08%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -5,15 +5,16 @@ public:
         sort(boxTypes.begin(), boxTypes.end(), [](vector<int>& a, vector<int>& b){
             return a[1] > b[1];
         });
+        
         int i = 0, res = 0;
+        
         while(truckSize > 0 && i<boxTypes.size()){
             
             res += min(truckSize, boxTypes[i][0])*boxTypes[i][1];
-            truckSize = truckSize - min(truckSize, boxTypes[i][0]); 
+            truckSize -= min(truckSize, boxTypes[i][0]);
             
             i++;
         }
-        
         return res;
     }
 };
