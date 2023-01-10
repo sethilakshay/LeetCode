ebe5c7949d29a8be660cf9f22bd46648e3a00cf7
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2023-01-10 05:09:46 -0500

    Time: 66 ms (84.09%), Space: 45.2 MB (35.29%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,81 +1,50 @@
 class Trie {
 public:
-    struct TrieNode {
+    Trie() {
+    }
+    
+    struct TrieNode{
         TrieNode* child[26] = {NULL};
         bool isWord = false;
     };
-    
-    Trie(){
-        
-    }
     
     TrieNode* root = new TrieNode();
     
     void insert(string word) {
         
         TrieNode* temp = root;
-        
-        for(int i=0; i<word.size(); i++){
-            int idx = word[i] - 'a';
-            
-            if(temp->child[idx] == NULL)
-                temp->child[idx] = new TrieNode();
-            
-            temp = temp->child[idx];
+        for(char c: word){
+            if(temp->child[c-'a'] == NULL){
+                temp->child[c-'a'] = new TrieNode();
+            }
+            temp = temp->child[c-'a'];
         }
-        
         temp->isWord = true;
-        
     }
     
     bool search(string word) {
         
         TrieNode* temp = root;
-        
-        for(int i=0; i<word.size(); i++){
-            int idx = word[i] - 'a';
-            
-            if(temp->child[idx] == NULL)
-                return false;
-            
-            temp = temp->child[idx];
-        }
-        
-        return temp->isWord;
-    }
+        for(char c: word){
     
-    bool search_deep(TrieNode* root){
-        //Base Case
-        if(root == NULL)
-            return false;
-        
-        if(root->isWord == true)
-            return true;
-        
-        //Recursion
-        for(int i=0; i<26; i++){
-            
-            if(search_deep(root->child[i])){
-                return true;
+            if(temp->child[c-'a'] == NULL){
+                return false;
             }
+            temp = temp->child[c-'a'];
         }
-        return false;
+        return temp->isWord;
     }
     
     bool startsWith(string prefix) {
         
         TrieNode* temp = root;
-        
-        for(int i=0; i<prefix.size(); i++){
-            int idx = prefix[i] - 'a';
-            
-            if(temp->child[idx] == NULL)
+        for(char c: prefix){
+            if(temp->child[c-'a'] == NULL){
                 return false;
-            
-            temp = temp->child[idx];
+            }
+            temp = temp->child[c-'a'];
         }
-        
-        return search_deep(temp);
+        return true;
     }
 };
 
