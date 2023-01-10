5ed84572b0d1191ec495cbd3759e047871849ec7
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2023-01-10 18:47:42 -0500

    Time: 7 ms (73.74%), Space: 10.5 MB (88.45%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -7,30 +7,26 @@ public:
             return;
         }
         
-        if(target < 0 || idx == candidates.size()){
+        if(target<0 || idx == candidates.size()){
             return;
         }
         
         // Recursive Case
-        
-        // Include idx in curr;
         curr.push_back(candidates[idx]);
         findSum(idx+1, candidates, target - candidates[idx], curr, res);
         curr.pop_back();
         
-        // Exclude
         int i = idx;
-        while(i<candidates.size() && candidates[i]==candidates[idx]){
+        while(i<candidates.size() && candidates[i] == candidates[idx]){
             i++;
         }
         findSum(i, candidates, target, curr, res);
-        
-        return;
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         
-        vector<int> curr;
         vector<vector<int>> res;
+        vector<int> curr;
+        
         sort(candidates.begin(), candidates.end());
         
         findSum(0, candidates, target, curr, res);
