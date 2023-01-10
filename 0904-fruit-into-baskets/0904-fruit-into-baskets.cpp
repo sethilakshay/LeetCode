deb57bcc55ea90f779fe65f335721a07400f9744
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2023-01-10 04:49:12 -0500

    Time: 119 ms (96.82%), Space: 66.1 MB (95.46%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -2,20 +2,23 @@ class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
         
-        int idx1 = 0, idx2 = -1, res = 0, count = 0;
+        int idx1 = 0, idx2 = -1, res = 0, cnt = 0;
         
         for(int i=0; i<fruits.size(); i++){
-            count++;
-            if(fruits[i] == fruits[idx1]){
+            cnt++;
+            if(fruits[idx1] == fruits[i]){
                 idx1 = i;
-            } else if(idx2 == -1 || fruits[i] == fruits[idx2]){
-                idx2 = i;
-            } else {
-                count = abs(idx1 - idx2) + 1;
-                idx1 = i-1;
+            }
+            else if(idx2 == -1 || fruits[idx2] == fruits[i]){
                 idx2 = i;
             }
-            res = max(res, count);
+            else{
+                cnt = abs(idx2 - idx1) + 1;
+                
+                idx2 = i;
+                idx1 = i-1;
+            }
+            res = max(res, cnt);
         }
         return res;
     }
