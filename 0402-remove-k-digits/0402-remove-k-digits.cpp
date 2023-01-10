eaad2f63097db1ba9f12eedcc2ebd4cf282be2cd
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2023-01-10 12:48:25 -0500

    Time: 11 ms (88.24%), Space: 8.8 MB (32.72%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    string removeKdigits(string& num, int k) {
+    string removeKdigits(string num, int k) {
         if(k >= num.size()){
             return "0";
         }
         
-        // Maintain a monotonic stack
+        int n = num.size(), i = 1;
         stack<char> stk;
         stk.push(num[0]);
         
-        for(int i=1; i<num.size(); i++){
+        while(i<n){
+            
             while(!stk.empty() && stk.top() > num[i] && k > 0){
-                k--;
                 stk.pop();
+                k--;
             }
             
             if(stk.size() == 1 && stk.top() == '0'){
@@ -20,21 +21,20 @@ public:
             }
             
             stk.push(num[i]);
+            i++;
         }
         
-        string res = "";
-        while(!stk.empty() && k > 0){
+        while(!stk.empty() && k>0){
             stk.pop();
             k--;
         }
         
+        string res = "";
         while(!stk.empty()){
             res.push_back(stk.top());
             stk.pop();
         }
-        
         reverse(res.begin(), res.end());
-        
         return res == "" ? "0" : res;
     }
 };
