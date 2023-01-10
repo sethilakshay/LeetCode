4dca5c2be96f7d4a4661c740ef6bc9bb726d179b
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2023-01-10 12:17:41 -0500

    Time: 3 ms (96.48%), Space: 8.2 MB (79.34%) - LeetHub

--- 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.cpp
+++ 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.cpp
@@ -7,7 +7,6 @@ public:
         vector<vector<int>> res(mat_1r, vector<int>(mat_2c, 0));
         
         for(int i=0; i<mat_1r; i++){
-            
             for(int j=0; j<mat_1c_2r; j++){
                 
                 if(mat1[i][j] == 0){
@@ -16,10 +15,9 @@ public:
                 
                 for(int k=0; k<mat_2c; k++){
                     res[i][k] += mat1[i][j]*mat2[j][k];
-                }   
+                }
             }
         }
-        
         return res;
     }
 };
