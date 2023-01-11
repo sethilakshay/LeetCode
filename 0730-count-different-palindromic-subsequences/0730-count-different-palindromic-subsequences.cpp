146e966d4b3aeddbdf677513b0cc818a23f410e4
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2023-01-11 03:38:40 -0500

    Time: 159 ms (80.80%), Space: 36.5 MB (40.63%) - LeetHub

--- 0730-count-different-palindromic-subsequences/0730-count-different-palindromic-subsequences.cpp
+++ 0730-count-different-palindromic-subsequences/0730-count-different-palindromic-subsequences.cpp
@@ -2,33 +2,29 @@ class Solution {
 public:
     int countPalindromicSubsequences(string s) {
         
-        int n = s.size(), mod = 1e9+7;
-        
-        vector<vector<int>> dp(n, vector<int>(n, 0));
-
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int> (n, 0));
+        long mod = 1e9+7;
         
         for(int i=n-1; i>=0; i--){
             dp[i][i] = 1;
-            
             for(int j=i+1; j<n; j++){
                 
                 if(s[i] != s[j]){
-                    dp[i][j] = dp[i][j-1] + dp[i+1][j] - dp[i+1][j-1];
+                    dp[i][j] = dp[i+1][j] + dp[i][j-1] - dp[i+1][j-1];
                 }
-                // if s[i] == s[j] then 3 cases
                 else{
                     dp[i][j] = 2*dp[i+1][j-1];
+                    
                     int left = i+1, right = j-1;
                     
                     while(left <= j-1 && s[left] != s[i]){
                         left++;
                     }
-                    
                     while(i+1 <= right && s[right] != s[j]){
                         right--;
                     }
                     
-                    // Case 1: means no char = s[i] in Palindrome
                     if(left > right){
                         dp[i][j] += 2;
                     }
@@ -39,7 +35,8 @@ public:
                         dp[i][j] -= dp[left+1][right-1];
                     }
                 }
-                dp[i][j] %= mod;
+                
+                dp[i][j] = dp[i][j]%mod;
                 if(dp[i][j] < 0){
                     dp[i][j] += mod;
                 }
