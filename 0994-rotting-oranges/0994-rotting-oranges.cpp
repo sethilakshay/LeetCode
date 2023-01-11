b4305e28d9acab929d12686f66fd1406069fcbc0
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2023-01-11 02:42:31 -0500

    Time: 8 ms (72.86%), Space: 13.1 MB (83.81%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -2,42 +2,17 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         
-        int fresh = 0, res = -1;
-        queue<pair<int, int>> q;
-        int row = grid.size(), col = grid[0].size();
+        int row = grid.size(), col = grid[0].size(), fresh = 0;
+        queue<pair<int, int>> rottingQ;
         
         for(int i=0; i<row; i++){
             for(int j=0; j<col; j++){
+                
                 if(grid[i][j] == 1){
                     fresh++;
                 }
                 else if(grid[i][j] == 2){
-                    q.push({i, j});
-                }
-            }
-        }
-        
-        int curr = 0;
-        vector<pair<int, int>> directions = {{0,1}, {0,-1}, {1,0}, {-1,0}};
-        
-        while(!q.empty()){
-            
-            int n = q.size();
-            res++;
-            for(int i=0; i<n; i++){
-                int c_row = q.front().first;
-                int c_col = q.front().second;
-                q.pop();
-                
-                for(int j=0; j<directions.size(); j++){
-                    int m_row = c_row + directions[j].first;
-                    int m_col = c_col + directions[j].second;
-                    
-                    if(m_row>=0 && m_row<row && m_col>=0 && m_col<col && grid[m_row][m_col] == 1){
-                        grid[m_row][m_col] = 2;
-                        q.push({m_row, m_col});
-                        curr++;
-                    }
+                    rottingQ.push({i, j});
                 }
             }
         }
@@ -46,11 +21,34 @@ public:
             return 0;
         }
         
-        if(curr != fresh){
+        int res = -1, num_fresh = 0;
+        vector<pair<int, int>> directions = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+        while(!rottingQ.empty()){
+            int n = rottingQ.size();
+            
+            for(int i=0; i<n; i++){
+                int curr_r = rottingQ.front().first;
+                int curr_c = rottingQ.front().second;
+                rottingQ.pop();
+                
+                for(int k=0; k<directions.size(); k++){
+                    int mod_r = curr_r + directions[k].first;
+                    int mod_c = curr_c + directions[k].second;
+                    
+                    if(mod_r>=0 && mod_r<row && mod_c>=0 && mod_c<col && grid[mod_r][mod_c]==1){
+                        grid[mod_r][mod_c] = 2;
+                        rottingQ.push({mod_r, mod_c});
+                        num_fresh++;
+                    }
+                }
+            }
+            res++;
+        }
+        
+        if(num_fresh != fresh){
             return -1;
         }
         
         return res;
-        
     }
 };
