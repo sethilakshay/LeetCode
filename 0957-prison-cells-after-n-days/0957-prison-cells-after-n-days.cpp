71e1ceb124fdf84dc449b2746498b4bf42f35365
Lakshay Sethi <59091360+sethilakshay@users.noreply.github.com>
2023-01-11 22:45:35 -0500

    Time: 0 ms (100.00%), Space: 12.6 MB (18.66%) - LeetHub

--- 0957-prison-cells-after-n-days/0957-prison-cells-after-n-days.cpp
+++ 0957-prison-cells-after-n-days/0957-prison-cells-after-n-days.cpp
@@ -5,8 +5,8 @@ public:
         
         for(int i=0; i<n; i++){
             string s (cells.begin(), cells.end());
-            
-            if (hash_map.find(s) != hash_map.end()){    //When true, that means the loop has started repeating
+            //When true, means the loop started repeating
+            if (hash_map.find(s) != hash_map.end()){
                 int cycle_l = i - hash_map[s];
                 int rem_days = (n-i)%cycle_l;
                 return prisonAfterNDays(cells, rem_days);
@@ -15,15 +15,10 @@ public:
             else{
                 hash_map[s] = i;
                 
-                vector<int> temp;
-                temp.push_back(0);  //The first Position will always be zero
-                
+                vector<int> temp(cells.size(), 0);
                 for(int j=1; j<cells.size()-1; j++){    //Simulating the remaining positions
-                    int num = s[j-1] == s[j+1] ? 1 : 0;
-                    temp.push_back(num);
+                    temp[j] = s[j-1] == s[j+1] ? 1 : 0;
                 }
-                temp.push_back(0);  //The last Position will always be zero
-                
                 cells = temp;
             }
         }
